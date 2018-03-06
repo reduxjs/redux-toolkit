2e76d386ee94d7f274dd2718d4f3a1fc0d69dced
Héliton Nordt <hnordt@hnordt.com>
2018-03-06 19:09:43 +0000

    Pass error/meta to case reducers, support non-FSAs

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,14 +1,19 @@
 import createNextState from "immer";
+import { isFSA } from "flux-standard-action";
 
 export function createReducer(initialState, actionsMap) {
     return function(state = initialState, action) {
-        const {type, payload} = action;
+        const {type, payload, ...rest} = action;
 
         return createNextState(state, draft => {
             const caseReducer = actionsMap[type];
 
             if(caseReducer) {
-                return caseReducer(draft, payload);
+                if (isFSA(action)) {
+                    return caseReducer(draft, payload, rest);
+                }
+
+                return caseReducer(draft,  rest);
             }
 
             return draft;
