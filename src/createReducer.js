38df46449dbbd92bc6ac17ab769385e14ab05bc4
Héliton Nordt <hnordt@hnordt.com>
2018-03-07 01:51:29 +0000

    Pass the whole action to caseReducer

--- src/createReducer.js
+++ src/createReducer.js
@@ -2,13 +2,11 @@ import createNextState from "immer";
 
 export function createReducer(initialState, actionsMap) {
     return function(state = initialState, action) {
-        const {type, payload} = action;
-
         return createNextState(state, draft => {
-            const caseReducer = actionsMap[type];
+            const caseReducer = actionsMap[action.type];
 
             if(caseReducer) {
-                return caseReducer(draft, payload);
+                return caseReducer(draft, action);
             }
 
             return draft;
