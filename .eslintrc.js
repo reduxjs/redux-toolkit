945dbb53014addf594e4398673cd980a3f4f16cb
Nick McCurdy <nick@nickmccurdy.com>
2018-03-10 04:19:13 -0500

    Format JavaScript and Markdown files with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,5 @@ module.exports = {
 
   rules: {
     'jsx-a11y/href-no-hash': 'off'
-  },
+  }
 }
--- src/isPlainObject.js
+++ src/isPlainObject.js
@@ -3,12 +3,12 @@
  * @returns {boolean} True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj) {
-    if (typeof obj !== 'object' || obj === null) return false
+  if (typeof obj !== 'object' || obj === null) return false
 
-    let proto = obj
-    while (Object.getPrototypeOf(proto) !== null) {
-        proto = Object.getPrototypeOf(proto)
-    }
+  let proto = obj
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto)
+  }
 
-    return Object.getPrototypeOf(obj) === proto
+  return Object.getPrototypeOf(obj) === proto
 }
