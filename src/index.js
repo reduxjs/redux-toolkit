fb54d0c28f680ba31d50746413f4653b6a189718
Edivan Silva <codedavinci.now@gmail.com>
2018-03-20 20:42:15 -0700

    Expose compose function from redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ export { configureStore, getDefaultMiddleware } from './configureStore'
 export { createReducer } from './createReducer'
 
 export { default as createNextState } from 'immer'
-export { combineReducers } from 'redux'
+export { combineReducers, compose } from 'redux'
 export { default as createSelector } from 'selectorator'
