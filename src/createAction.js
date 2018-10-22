7d4208289d720ada0d99a9ed6bc69aad7ce386ed
Martijn <martijn@touchtribe.nl>
2018-10-22 19:23:21 +0200

    Add payloadCreator parameter to createAction

--- src/createAction.js
+++ src/createAction.js
@@ -1,7 +1,14 @@
-export function createAction(type) {
-  const action = payload => ({
+import identity from './identity'
+import isFunction from './isFunction'
+
+export function createAction(type, payloadCreator) {
+  if (!isFunction(payloadCreator)) {
+    payloadCreator = identity
+  }
+
+  const action = (...args) => ({
     type,
-    payload
+    payload: payloadCreator(...args)
   })
   action.toString = () => `${type}`
   return action
