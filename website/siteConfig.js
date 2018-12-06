362740d6ad2ca88bd6095f956b240a2cfe89aa69
Mark Erikson <mark@isquaredsoftware.com>
2018-12-05 23:42:23 -0500

    Update site config with docs pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,8 +9,8 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'Redux Starter kit', // Title for your website.
-  tagline: 'Redux Starter kit Documentation',
+  title: 'Redux Starter Kit', // Title for your website.
+  tagline: 'Redux Starter Kit Documentation',
   url: 'https://redux-starter-kit.js.org', // Your website URL
   baseUrl: '/',
   docsUrl: '',
@@ -30,7 +30,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: 'introduction/quick-start', label: 'Quick Start' },
-    { doc: 'api', label: 'API' }
+    { doc: 'api/configureStore', label: 'API' }
   ],
 
   /* path to images for header/footer */
@@ -59,7 +59,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} The Redux Team`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
