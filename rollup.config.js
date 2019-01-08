59a4d446c8bd5f76946d7152f72d8a145ebe2491
Denis Washington <denis@denisw.de>
2019-01-08 23:31:26 +0100

    Port index.js to TS and add index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 
-const input = 'src/index'
+const input = './src/index.ts'
 const exclude = 'node_modules/**'
 
 const extensions = ['.ts', '.js']
