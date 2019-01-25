26b94d4caba5f9964affb40560d62f99b641debc
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-25 02:46:28 +0100

    Alternate: Add ability for slices to listen to other actions (#86)
    Implemented my proposed changes from https://github.com/reduxjs/redux-starter-kit/pull/83#issuecomment-456379590
    
    @denisw  @markerikson your inputs are appreciated
    
    *Note: this PR is for the `feature/other-slice-action` branch not `master`*
    
    * ~~Removed `getType()` utility~~
    
    * ~~`slice` is no longer attached to `actionsMap` and `reducerMap`~~
    
    * ~~Removed `reducerMap` and directly forward `reducers` to `createReducer`~~
    
    - [x] `createAction()` action creators `type` property returns a `string literal` for better type saftey
    
    - [x] Fixed tests
    
    - [x] Added tests

--- src/createAction.ts
+++ src/createAction.ts
@@ -18,7 +18,7 @@ export interface PayloadAction<P = any, T extends string = string>
 export interface PayloadActionCreator<P = any, T extends string = string> {
   (): Action<T>
   (payload: P): PayloadAction<P, T>
-  type: string
+  type: T
 }
 
 /**
@@ -32,14 +32,14 @@ export interface PayloadActionCreator<P = any, T extends string = string> {
  */
 export function createAction<P = any, T extends string = string>(
   type: T
-): PayloadActionCreator<P> {
+): PayloadActionCreator<P, T> {
   function actionCreator(): Action<T>
   function actionCreator(payload: P): PayloadAction<P, T>
   function actionCreator(payload?: P): Action<T> | PayloadAction<P, T> {
     return { type, payload }
   }
 
-  actionCreator.toString = () => `${type}`
+  actionCreator.toString = (): T => `${type}` as T
 
   actionCreator.type = type
 
