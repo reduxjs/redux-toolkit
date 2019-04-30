bf2f3769c44f02330ac92cadf3139ebca89b42f9
Mark Erikson <mark@isquaredsoftware.com>
2019-04-29 19:43:48 -0700

    Fix accidentally removed re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+export { combineReducers, compose } from 'redux'
+export { default as createNextState } from 'immer'
+export { default as createSelector } from 'selectorator'
+
 export * from './configureStore'
 export * from './createAction'
 export * from './createReducer'
