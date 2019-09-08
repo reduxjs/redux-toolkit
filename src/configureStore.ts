4cae117d7ffd0613586d2f6da9e9070c25a362f1
Mark Erikson <mark@isquaredsoftware.com>
2019-09-08 18:03:51 -0400

    Rename EnhancerOptions to DevToolsOptions

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -12,7 +12,10 @@ import {
   Store,
   DeepPartial
 } from 'redux'
-import { composeWithDevTools, EnhancerOptions } from 'redux-devtools-extension'
+import {
+  composeWithDevTools,
+  EnhancerOptions as DevToolsOptions
+} from 'redux-devtools-extension'
 import { ThunkDispatch } from 'redux-thunk'
 
 import isPlainObject from './isPlainObject'
@@ -43,12 +46,13 @@ export interface ConfigureStoreOptions<S = any, A extends Action = AnyAction> {
   /**
    * Whether to enable Redux DevTools integration. Defaults to `true`.
    *
-   * Additional configuration can be done by passing enhancer options
+   * Additional configuration can be done by passing Redux DevTools options
    */
-  devTools?: boolean | EnhancerOptions
+  devTools?: boolean | DevToolsOptions
 
   /**
-   * The initial state. You may optionally specify it to hydrate the state
+   * The initial state, same as Redux's createStore.
+   * You may optionally specify it to hydrate the state
    * from the server in universal apps, or to restore a previously serialized
    * user session. If you use `combineReducers()` to produce the root reducer
    * function (either directly or indirectly by passing an object as `reducer`),
@@ -61,8 +65,12 @@ export interface ConfigureStoreOptions<S = any, A extends Action = AnyAction> {
   preloadedState?: DeepPartial<S extends any ? S : S>
 
   /**
-   * The store enhancers to apply. See Redux's `createStore()`. If you only
-   * need to add middleware, you can use the `middleware` parameter instaead.
+   * The store enhancers to apply. See Redux's `createStore()`.
+   * All enhancers will be included before the DevTools Extension enhancer.
+   * If you need to customize the order of enhancers, supply a callback
+   * function that will receive the original array (ie, `[applyMiddleware]`),
+   * and should return a new array (such as `[applyMiddleware, offline]`).
+   * If you only need to add middleware, you can use the `middleware` parameter instaead.
    */
   enhancers?: StoreEnhancer[] | ConfigureEnhancersCallback
 }
