0b25b37f479ebf642b74c857edef401788b41311
Mark Erikson <mark@isquaredsoftware.com>
2019-10-17 23:40:01 -0400

    Silence a bizarre TSLint "rule not found" error
    Prevents a whole bunch of repeats of this error:
    
    Definition for rule '@typescript-eslint/no-angle-bracket-type-assertion'
    was not found @typescript-eslint/no-angle-bracket-type-assertion

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     'jsx-a11y/href-no-hash': 'off',
     // Taken care of by TypeScript's `noUnusedLocals` / `noUnusedParameters`
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    // Silence some bizarre "rule not found" TSLint error
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'off'
   }
 }
