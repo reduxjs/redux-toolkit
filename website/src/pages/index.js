6aecd8d033609b35ed565f1cf531c87dcdf92ab3
Wei Gao <wgao19@gmail.com>
2019-11-09 23:27:04 +0800

    remove lightBackground class that breaks dark mode

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,42 +5,64 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
-
-// const siteConfig = require(`../../docusaurus.config.js`)
+import React from 'react'
+import classnames from 'classnames'
+import Layout from '@theme/Layout'
+import useTheme from '@theme/hooks/useTheme'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import styles from './styles.module.css'
 
 const features = [
   {
-    content: <p>Includes utilities to simplify common use cases like <strong>store setup, creating reducers, immutable update logic</strong>, and more.</p>,
+    content: (
+      <p>
+        Includes utilities to simplify common use cases like{' '}
+        <strong>store setup, creating reducers, immutable update logic</strong>,
+        and more.
+      </p>
+    ),
     image: 'img/noun_snap_1984955.svg',
     imageAlign: 'top',
     title: 'Simple'
   },
   {
-    content: <p>Provides <strong>good defaults for store setup out of the box</strong>, and includes <strong>the most commonly used Redux addons built-in</strong>.</p>,
+    content: (
+      <p>
+        Provides <strong>good defaults for store setup out of the box</strong>,
+        and includes{' '}
+        <strong>the most commonly used Redux addons built-in</strong>.
+      </p>
+    ),
     image: 'img/noun_Brain_1551075.svg',
     imageAlign: 'top',
     title: 'Opinionated'
   },
   {
-    content: <p>Takes inspiration from libraries like Immer and Autodux to let you <strong>write "mutative" immutable update logic</strong>, and even <strong>create entire "slices" of state automatically</strong>.</p>,
+    content: (
+      <p>
+        Takes inspiration from libraries like Immer and Autodux to let you{' '}
+        <strong>write "mutative" immutable update logic</strong>, and even{' '}
+        <strong>create entire "slices" of state automatically</strong>.
+      </p>
+    ),
     image: 'img/noun_Bicep_1338504.svg',
     imageAlign: 'top',
     title: 'Powerful'
   },
   {
-    content: <p>Lets you focus on the core logic your app needs, so you can <strong>do more work with less code</strong>.</p>,
+    content: (
+      <p>
+        Lets you focus on the core logic your app needs, so you can{' '}
+        <strong>do more work with less code</strong>.
+      </p>
+    ),
     image: 'img/noun_Checklist_437165.svg',
     imageAlign: 'top',
     title: 'Effective'
   }
-];
+]
 
 const otherLiraries = [
   {
@@ -55,29 +77,35 @@ const otherLiraries = [
     link: 'https://react-redux.js.org',
     image: 'img/external-link-square-alt-solid.svg'
   }
-];
+]
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
+  const [theme] = useTheme()
+  console.log('theme is', theme)
 
   return (
-    <Layout
-      title={siteConfig.title}
-      description={siteConfig.tagline}
-    >
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <div className={styles.title}>
-            <img src="img/redux_white.svg" alt="Redux logo" width="100" height="100" />
-            <h1 className={`${styles.projectTitle} hero__title`}>{siteConfig.title}</h1>
+            <img
+              src="img/redux_white.svg"
+              alt="Redux logo"
+              width="100"
+              height="100"
+            />
+            <h1 className={`${styles.projectTitle} hero__title`}>
+              {siteConfig.title}
+            </h1>
           </div>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
                 'button button--secondary button--lg',
-                styles.getStarted,
+                styles.getStarted
               )}
               to={useBaseUrl('introduction/quick-start')}
             >
@@ -88,7 +116,7 @@ function Home() {
       </header>
       <main>
         {features && features.length && (
-          <section className={[styles.features, styles.lightBackground].join(' ')}>
+          <section className={classnames(styles.features)}>
             <div className={`container ${styles.featureBlock}`}>
               <div className="row">
                 {features.map(({ image, title, content }, idx) => (
@@ -105,7 +133,9 @@ function Home() {
                         />
                       </div>
                     )}
-                    <h2 className={`text--center ${styles.featureTitle}`}>{title}</h2>
+                    <h2 className={`text--center ${styles.featureTitle}`}>
+                      {title}
+                    </h2>
                     {content}
                   </div>
                 ))}
@@ -118,12 +148,17 @@ function Home() {
             <div className="container">
               <div className="row">
                 <div className="col">
-                  <h2 className={`text--center ${styles.secondTitle}`}>Other Libraries from the Redux Team</h2>
+                  <h2 className={`text--center ${styles.secondTitle}`}>
+                    Other Libraries from the Redux Team
+                  </h2>
                 </div>
               </div>
               <div className="row">
                 {otherLiraries.map(({ image, title, content, link }, idx) => (
-                  <div key={idx} className={classnames('col col--6', styles.feature)}>
+                  <div
+                    key={idx}
+                    className={classnames('col col--6', styles.feature)}
+                  >
                     <h2 className="text--center">
                       <a href={link}>
                         {title}
@@ -131,7 +166,7 @@ function Home() {
                       </a>
                     </h2>
                     <p className="text--center">{content}</p>
-                  </div> 
+                  </div>
                 ))}
               </div>
             </div>
@@ -139,7 +174,7 @@ function Home() {
         )}
       </main>
     </Layout>
-  );
+  )
 }
 
-export default Home;
+export default Home
