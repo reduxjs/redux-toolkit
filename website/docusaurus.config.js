20f5e72b36d388fb0332b7a7de8208afe66c24ff
yangshun <tay.yang.shun@gmail.com>
2019-11-13 16:14:35 -0800

    tweaks

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,18 +8,19 @@ module.exports = {
         docs: {
           path: '../docs',
           sidebarPath: require.resolve('./sidebars.json'),
-          routeBasePath: '/'
+          routeBasePath: '/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
-        }
-      }
-    ]
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
   ],
   projectName: 'redux-toolkit',
   baseUrl: '/',
   favicon: 'img/favicon/favicon.ico',
-  tagline: 'The official, opinionated, batteries-included toolset for efficient Redux development',
+  tagline:
+    'The official, opinionated, batteries-included toolset for efficient Redux development',
   title: 'Redux Toolkit',
   url: 'https://redux-toolkit.js.org',
   customFields: {
@@ -27,13 +28,13 @@ module.exports = {
   },
   themeConfig: {
     prism: {
-      theme: require('./src/js/monokaiTheme')
+      theme: require('./src/js/monokaiTheme'),
     },
     footer: {
       style: 'dark',
       logo: {
         alt: 'Redux Logo',
-        src: 'img/redux_white.svg'
+        src: 'img/redux_white.svg',
       },
       links: [
         {
@@ -41,68 +42,67 @@ module.exports = {
           items: [
             {
               label: 'Quick Start',
-              to: 'introduction/quick-start'
+              to: 'introduction/quick-start',
             },
             {
               label: 'API Reference',
-              to: 'api/configureStore'
-            }
-          ]
+              to: 'api/configureStore',
+            },
+          ],
         },
         {
           title: 'Community',
           items: [
             {
               label: 'Stack Overflow',
-              href: 'http://stackoverflow.com/questions/tagged/redux'
+              href: 'http://stackoverflow.com/questions/tagged/redux',
             },
             {
               label: 'Discord',
-              href: 'https://discord.gg/0ZcbPKXt5bZ6au5t'
-            }
-          ]
+              href: 'https://discord.gg/0ZcbPKXt5bZ6au5t',
+            },
+          ],
         },
         {
           title: 'Social',
           items: [
             {
               label: 'GitHub',
-              href: 'https://www.github.com/reduxjs/redux-toolkit'
-            }
-          ]
-        }
+              href: 'https://www.github.com/reduxjs/redux-toolkit',
+            },
+          ],
+        },
       ],
-      copyright: `Copyright (c) 2015-present Dan Abramov and the Redux documentation authors.`
+      copyright: `Copyright (c) 2015-present Dan Abramov and the Redux documentation authors.`,
     },
     image: 'img/redux-logo-landscape.png',
-    // twitterImage: 'img/redux-logo-twitter.png',
     navbar: {
-      title: 'Redux Starter Kit',
+      title: 'Redux Toolkit',
       logo: {
         alt: 'Redux Logo',
-        src: 'img/redux.svg'
+        src: 'img/redux.svg',
       },
       links: [
         {
           to: 'introduction/quick-start',
           label: 'Quick Start',
-          position: 'right'
+          position: 'right',
         },
         { to: 'api/configureStore', label: 'API', position: 'right' },
         {
           href: 'https://www.github.com/reduxjs/redux-starter-kit',
           label: 'GitHub',
-          position: 'right'
-        }
-      ]
+          position: 'right',
+        },
+      ],
     },
     algolia: {
       apiKey: '82d838443b672336bf63cab4772d9eb4',
       indexName: 'redux-starter-kit',
-      algoliaOptions: {}
+      algoliaOptions: {},
     },
     googleAnalytics: {
-      trackingID: 'UA-130598673-3'
-    }
-  }
+      trackingID: 'UA-130598673-3',
+    },
+  },
 }
--- website/src/js/monokaiTheme.js
+++ website/src/js/monokaiTheme.js
@@ -1,62 +1,44 @@
 module.exports = {
-  "plain": {
-    "color": "#f8f8f2",
-    "backgroundColor": "#272822"
+  plain: {
+    color: '#f8f8f2',
+    backgroundColor: '#272822',
   },
-  "styles": [
+  styles: [
     {
-      "types": [
-        "comment"
-      ],
-      "style": {
-        "color": "rgb(136, 132, 111)"
-      }
+      types: ['comment'],
+      style: {
+        color: 'rgb(136, 132, 111)',
+      },
     },
     {
-      "types": [
-        "string",
-        "changed"
-      ],
-      "style": {
-        "color": "rgb(230, 219, 116)"
-      }
+      types: ['string', 'changed'],
+      style: {
+        color: 'rgb(230, 219, 116)',
+      },
     },
     {
-      "types": [
-        "punctuation",
-        "tag",
-        "deleted"
-      ],
-      "style": {
-        "color": "rgb(249, 38, 114)"
-      }
+      types: ['punctuation', 'tag', 'deleted'],
+      style: {
+        color: 'rgb(249, 38, 114)',
+      },
     },
     {
-      "types": [
-        "number",
-        "builtin"
-      ],
-      "style": {
-        "color": "rgb(174, 129, 255)"
-      }
+      types: ['number', 'builtin'],
+      style: {
+        color: 'rgb(174, 129, 255)',
+      },
     },
     {
-      "types": [
-        "variable"
-      ],
-      "style": {
-        "color": "rgb(248, 248, 242)"
-      }
+      types: ['variable'],
+      style: {
+        color: 'rgb(248, 248, 242)',
+      },
     },
     {
-      "types": [
-        "function",
-        "attr-name",
-        "inserted"
-      ],
-      "style": {
-        "color": "rgb(166, 226, 46)"
-      }
-    }
-  ]
-};
+      types: ['function', 'attr-name', 'inserted'],
+      style: {
+        color: 'rgb(166, 226, 46)',
+      },
+    },
+  ],
+}
