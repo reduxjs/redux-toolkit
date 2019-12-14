0f4b0e0c670091e05448593c9af2d5b10f6a540e
Alex Ketch <alex-ketch@users.noreply.github.com>
2019-12-13 22:35:19 -0500

    Fix module bundling & ESM runtime error (#280)

--- tsdx.config.js
+++ tsdx.config.js
@@ -15,19 +15,26 @@ module.exports = {
       case 'umd':
         delete config.external
         config.output.indent = false
-        config.plugins.push(
+        config.plugins.unshift(
           replace({
-            '// UMD-DEV-ONLY: ': ''
+            '// UMD-ONLY: ': '',
+            delimiters: ['', '']
           })
         )
-        config.plugins.push(
+        config.plugins.unshift(
           stripCode({
-            // Remove the `require()` import of RISI so we use the import statement
+            // Remove the `import` of RISI so we use the dynamic `require()` statement
             start_comment: 'START_REMOVE_UMD',
             end_comment: 'STOP_REMOVE_UMD'
           })
         )
         if (env === 'production') {
+          config.plugins.unshift(
+            stripCode({
+              start_comment: 'PROD_START_REMOVE_UMD',
+              end_comment: 'PROD_STOP_REMOVE_UMD'
+            })
+          )
           config.output.file = join(__dirname, pkg.unpkg)
         } else {
           config.output.file = config.output.file.replace(
