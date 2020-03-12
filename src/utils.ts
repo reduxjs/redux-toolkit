11d1bf7ae1eed9c9c1c1919175198dc44fc4b983
Mark Erikson <mark@isquaredsoftware.com>
2020-03-11 20:05:37 -0400

    Update src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,7 @@ export function getTimeMeasureUtils(maxDelay: number, fnName: string) {
     warnIfExceeded() {
       if (elapsed > maxDelay) {
         console.warn(`${fnName} took ${elapsed}ms, which is more than the warning threshold of ${maxDelay}ms. 
-If you are passing very large objects into your state, you might to disable the middleware as it might cause too much of a slowdown in development mode. 
+If your state or actions are very large, you may want to disable the middleware as it might cause too much of a slowdown in development mode. See https://redux-toolkit.js.org/api/getDefaultMiddleware for instructions.
 It is disabled in production builds, so you don't need to worry about that.`)
       }
     }
