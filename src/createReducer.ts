e736b17708e4c3f5e0529c817f67141b591b843c
Andrew Craswell <andcra@gmail.com>
2020-04-15 20:32:14 -0700

    Resolve linting errors

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -111,7 +111,8 @@ export function createReducer<S>(
         // we must already be inside a `createNextState` call, likely because
         // this is being wrapped in `createReducer` or `createSlice`.
         // It's safe to just pass the draft to the mutator.
-        return caseReducer(state, action)
+        const draft = state as Draft<S> // We can aassume this is already a draft
+        return caseReducer(draft, action) || state
       } else {
         // @ts-ignore createNextState() produces an Immutable<Draft<S>> rather
         // than an Immutable<S>, and TypeScript cannot find out how to reconcile
