3385e9960817614519dc944c1a0c6ffd952ce688
Andrew Craswell <ajcraswell@gmail.com>
2020-04-17 10:31:18 -0700

    Update src/createReducer.ts
    Co-Authored-By: Jonas Holst Damtoft <Jomik@users.noreply.github.com>

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -112,7 +112,7 @@ export function createReducer<S>(
         // this is being wrapped in `createReducer` or `createSlice`.
         // It's safe to just pass the draft to the mutator.
         const draft = state as Draft<S> // We can aassume this is already a draft
-        return caseReducer(draft, action) || state
+        return caseReducer(draft, action) ?? state
       } else {
         // @ts-ignore createNextState() produces an Immutable<Draft<S>> rather
         // than an Immutable<S>, and TypeScript cannot find out how to reconcile
