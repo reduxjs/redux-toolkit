87a48a9fa573f097bf302db0c8b23a7e00a2d41c
Matt Sutkowski <msutkowski@gmail.com>
2020-07-18 17:39:25 -0700

    Use system build option for es browser build

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,5 +1,11 @@
 const { join } = require('path')
 
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const babel = require('rollup-plugin-babel')
+const replace = require('@rollup/plugin-replace')
+const typescript = require('rollup-plugin-typescript2')
+const { terser } = require('rollup-plugin-terser')
+
 const stripCode = require('rollup-plugin-strip-code')
 
 const pkg = require('./package.json')
@@ -11,6 +17,38 @@ module.exports = {
     const { env, format } = options
     // eslint-disable-next-line default-case
     switch (format) {
+      case 'system':
+        const extensions = ['.ts']
+
+        config.input = 'src/index.ts'
+        config.output = {
+          file: 'dist/redux-toolkit.min.mjs',
+          format: 'es',
+          indent: false
+        }
+        config.plugins = [
+          nodeResolve({
+            extensions
+          }),
+          replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+          }),
+          typescript({
+            tsconfigOverride: { compilerOptions: { declaration: false } }
+          }),
+          babel({
+            extensions,
+            exclude: 'node_modules/**'
+          }),
+          terser({
+            compress: {
+              pure_getters: true,
+              unsafe: true,
+              unsafe_comps: true,
+              warnings: false
+            }
+          })
+        ]
       case 'umd':
         delete config.external
         config.output.indent = false
