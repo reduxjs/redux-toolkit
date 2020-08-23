55f526d246ce48bac1643161256e09695cda77ae
Lenz Weber <mail@lenzw.de>
2020-08-23 12:56:33 +0200

    remove test that was accidentally added

--- type-tests/files/createAction.typetest.ts
+++ type-tests/files/createAction.typetest.ts
@@ -347,19 +347,3 @@ function expectType<T>(p: T): T {
   type AnyPayload = ReturnType<typeof anyCreator>['payload']
   expectType<IsAny<AnyPayload, true, false>>(true)
 }
-
-{
-  const increment = createAction<number | undefined>('counter/increment')
-
-  let action = increment()
-  // { type: 'counter/increment' }
-
-  action = increment(3)
-  // returns { type: 'counter/increment', payload: 3 }
-
-  console.log(increment.toString())
-  // 'counter/increment'
-
-  console.log(`The action type is: ${increment}`)
-  // 'The action type is: counter/increment'
-}
