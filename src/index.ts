817e5fb34b386141f8af4eff0cf8acedd53ba93d
Lenz Weber <mail@lenzw.de>
2020-10-18 21:09:04 +0200

    basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import {
   PayloadAction,
-  Reducer,
-  AnyAction,
   configureStore,
+  createSlice,
+  createAsyncThunk,
+  ThunkAction,
+  Draft,
+  createNextState,
 } from '@reduxjs/toolkit';
 
 const resultType = Symbol();
 
-type NoInfer<T> = [T][T extends any ? 0 : never];
+// type NoInfer<T> = [T][T extends any ? 0 : never];
 
 interface BaseEndpointDefinition<QueryArg, InternalQueryArgs, ResultType> {
   query(arg: QueryArg): InternalQueryArgs;
@@ -58,6 +61,18 @@ type EndpointDefinitions = Record<
   EndpointDefinition<any, any, any, any>
 >;
 
+function isQueryDefinition(
+  e: EndpointDefinition<any, any, any, any>
+): e is QueryDefinition<any, any, any, any> {
+  return 'provides' in e;
+}
+
+function isMutationDefinition(
+  e: EndpointDefinition<any, any, any, any>
+): e is MutationDefinition<any, any, any, any> {
+  return 'invalidates' in e;
+}
+
 type EndpointBuilder<InternalQueryArgs, EntityTypes extends string> = {
   query<ResultType, QueryArg>(
     definition: QueryDefinition<
@@ -82,29 +97,63 @@ type Id<T> = { [K in keyof T]: T[K] } & {};
 type QueryActions<Definitions extends EndpointDefinitions> = {
   [K in keyof Definitions]: Definitions[K] extends QueryDefinition<
     infer QueryArg,
+    any,
+    any,
+    infer ResultType
+  >
+    ? (arg: QueryArg) => ThunkAction<ResultType, any, any, any>
+    : never;
+};
+
+type MutationActions<Definitions extends EndpointDefinitions> = {
+  [K in keyof Definitions]: Definitions[K] extends MutationDefinition<
+    infer QueryArg,
     infer InternalQueryArg,
     any,
     any
   >
     ? (arg: QueryArg) => PayloadAction<InternalQueryArg>
-    : Definitions[K];
+    : never;
 };
 
-type QueryStatus = 'uninitialized' | 'pending' | 'fulfilled' | 'rejected';
+enum QueryStatus {
+  uninitialized = 'uninitialized',
+  pending = 'pending',
+  fulfilled = 'fulfilled',
+  rejected = 'rejected',
+}
 
-type ResultSelectors<Definitions extends EndpointDefinitions, RootState> = {
-  [K in keyof Definitions]: Definitions[K] extends BaseEndpointDefinition<
+type QueryResultSelectors<
+  Definitions extends EndpointDefinitions,
+  RootState
+> = {
+  [K in keyof Definitions]: Definitions[K] extends QueryDefinition<
     infer QueryArg,
+    any,
     any,
     infer ResultType
   >
     ? (
         queryArg: QueryArg
-      ) => (state: RootState) => { status: QueryStatus; data: ResultType }
+      ) => (state: RootState) => QuerySubState<Definitions[K]>
     : never;
 };
 
-type QueryState = {};
+type MutationResultSelectors<
+  Definitions extends EndpointDefinitions,
+  RootState
+> = {
+  [K in keyof Definitions]: Definitions[K] extends MutationDefinition<
+    any,
+    any,
+    any,
+    infer ResultType
+  >
+    ? (
+        requestId: string
+      ) => (state: RootState) => MutationSubState<Definitions[K]>
+    : never;
+};
 
 type Hooks<Definitions extends EndpointDefinitions> = {
   [K in keyof Definitions]: Definitions[K] extends QueryDefinition<
@@ -131,91 +180,373 @@ type Hooks<Definitions extends EndpointDefinitions> = {
     : never;
 };
 
-function createApi<
+type Subscribers = string[];
+
+type QueryKeys<Definitions extends EndpointDefinitions> = {
+  [K in keyof Definitions]: Definitions[K] extends QueryDefinition<
+    any,
+    any,
+    any,
+    any
+  >
+    ? K
+    : never;
+}[keyof Definitions];
+type MutationKeys<Definitions extends EndpointDefinitions> = {
+  [K in keyof Definitions]: Definitions[K] extends MutationDefinition<
+    any,
+    any,
+    any,
+    any
+  >
+    ? K
+    : never;
+}[keyof Definitions];
+
+type QueryArgs<
+  D extends BaseEndpointDefinition<any, any, any>
+> = D extends BaseEndpointDefinition<infer QA, any, any> ? QA : unknown;
+type ResultType<
+  D extends BaseEndpointDefinition<any, any, any>
+> = D extends BaseEndpointDefinition<any, any, infer RT> ? RT : unknown;
+type EntityType<
+  D extends BaseEndpointDefinition<any, any, any>
+> = D extends QueryDefinition<any, any, infer T, any> ? T : string;
+
+type QuerySubState<D extends BaseEndpointDefinition<any, any, any>> =
+  | {
+      status: QueryStatus.uninitialized;
+      arg?: undefined;
+      data?: undefined;
+      subscribers: Subscribers;
+      resultingEntities: Array<EntityDescription<EntityType<D>>>;
+    }
+  | {
+      status: QueryStatus.pending;
+      arg: QueryArgs<D>;
+      data?: ResultType<D>;
+      subscribers: Subscribers;
+      resultingEntities: Array<EntityDescription<EntityType<D>>>;
+    }
+  | {
+      status: QueryStatus.rejected;
+      arg: QueryArgs<D>;
+      data?: ResultType<D>;
+      subscribers: Subscribers;
+      resultingEntities: Array<EntityDescription<EntityType<D>>>;
+    }
+  | {
+      status: QueryStatus.fulfilled;
+      arg: QueryArgs<D>;
+      data: ResultType<D>;
+      subscribers: Subscribers;
+      resultingEntities: Array<EntityDescription<EntityType<D>>>;
+    };
+
+type MutationSubState<D extends BaseEndpointDefinition<any, any, any>> =
+  | {
+      status: QueryStatus.uninitialized;
+      arg?: undefined;
+      data?: undefined;
+    }
+  | {
+      status: QueryStatus.pending;
+      arg: QueryArgs<D>;
+      data?: ResultType<D>;
+    }
+  | {
+      status: QueryStatus.rejected;
+      arg: QueryArgs<D>;
+      data?: ResultType<D>;
+    }
+  | {
+      status: QueryStatus.fulfilled;
+      arg: QueryArgs<D>;
+      data: ResultType<D>;
+    };
+
+type QueryState<D extends EndpointDefinitions> = {
+  queries: {
+    [K in QueryKeys<D>]?: {
+      [stringifiedArgs in string]?: QuerySubState<D[K]>;
+    };
+  };
+  mutations: {
+    [K in MutationKeys<D>]?: {
+      [requestId in string]?: MutationSubState<D[K]>;
+    };
+  };
+};
+
+function defaultSerializeQueryArgs(args: any) {
+  return JSON.stringify(args);
+}
+
+const __phantomType_ReducerPath = Symbol();
+interface QueryStatePhantomType<Identifier extends string> {
+  [__phantomType_ReducerPath]: Identifier;
+}
+
+// abuse immer to freeze default states
+const defaultQuerySubState = createNextState(
+  {},
+  (): QuerySubState<any> => {
+    return {
+      status: QueryStatus.uninitialized,
+      subscribers: [],
+      resultingEntities: [],
+    };
+  }
+);
+const defaultMutationSubState = createNextState(
+  {},
+  (): MutationSubState<any> => {
+    return {
+      status: QueryStatus.uninitialized,
+    };
+  }
+);
+
+export function createApi<
   InternalQueryArgs,
   Definitions extends EndpointDefinitions,
+  ReducerPath extends string,
   EntityTypes extends string = never
->(definition: {
+>({
+  baseQuery,
+  reducerPath,
+  serializeQueryArgs = defaultSerializeQueryArgs,
+  endpoints,
+}: {
   baseQuery(args: InternalQueryArgs): any;
   entityTypes: readonly EntityTypes[];
-  serializeQueryName?(method: string, args: InternalQueryArgs): string;
+  reducerPath: ReducerPath;
+  serializeQueryArgs?(args: InternalQueryArgs): string;
   endpoints(
     build: EndpointBuilder<InternalQueryArgs, EntityTypes>
   ): Definitions;
 }) {
-  type MutationActions<Definitions extends EndpointDefinitions> = {
-    [K in keyof Definitions]: Definitions[K] extends MutationDefinition<
-      infer QueryArg,
-      infer InternalQueryArg,
-      any,
-      any
-    >
-      ? (arg: QueryArg) => PayloadAction<InternalQueryArg>
-      : never;
+  serializeQueryArgs({} as any);
+
+  type State = QueryState<Definitions>;
+  type InternalState = QueryState<any>;
+  type RootState = {
+    [K in ReducerPath]: State & QueryStatePhantomType<ReducerPath>;
+  };
+  type InternalRootState = {
+    [K in ReducerPath]: InternalState;
   };
 
-  return {} as {
+  type QueryThunkArgs = {
+    endpoint: string;
+    serializedQueryArgs: string;
+    internalQueryArgs: InternalQueryArgs;
+  };
+
+  const queryThunk = createAsyncThunk<
+    unknown,
+    QueryThunkArgs,
+    { state: InternalRootState }
+  >(
+    `${reducerPath}/executeQuery`,
+    (arg) => {
+      return baseQuery(arg.internalQueryArgs);
+    },
+    {
+      condition(arg, { getState }) {
+        let requestState = getState()[reducerPath]?.queries?.[arg.endpoint]?.[
+          arg.serializedQueryArgs
+        ];
+        return requestState?.status !== 'pending';
+      },
+    }
+  );
+
+  type MutationThunkArgs = {
+    endpoint: string;
+    serializedQueryArgs: string;
+    internalQueryArgs: InternalQueryArgs;
+  };
+
+  const mutationThunk = createAsyncThunk<
+    unknown,
+    MutationThunkArgs,
+    { state: InternalRootState }
+  >(`${reducerPath}/executeMutation`, (arg) => {
+    return baseQuery(arg.internalQueryArgs);
+  });
+
+  const initialState: State = {
+    queries: {},
+    mutations: {},
+  };
+
+  function getQuerySubState(
+    state: Draft<State>,
+    {
+      meta: { arg },
+    }: { meta: { arg: { endpoint: string; serializedQueryArgs: string } } }
+  ) {
+    return (((state as InternalState).queries[arg.endpoint] ??= {})[
+      arg.serializedQueryArgs
+    ] ??= {
+      status: QueryStatus.uninitialized,
+      resultingEntities: [],
+      subscribers: [],
+    });
+  }
+
+  function getMutationSubState(
+    state: Draft<State>,
+    {
+      meta: { arg, requestId },
+    }: { meta: { arg: { endpoint: string }; requestId: string } }
+  ) {
+    return (((state as InternalState).mutations[arg.endpoint] ??= {})[
+      requestId
+    ] ??= {
+      status: QueryStatus.uninitialized,
+    });
+  }
+
+  const slice = createSlice({
+    name: `${reducerPath}/states`,
+    initialState: initialState as Id<
+      State & QueryStatePhantomType<ReducerPath>
+    >,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(queryThunk.pending, (draft, action) => {
+          Object.assign(getQuerySubState(draft, action), {
+            status: QueryStatus.pending,
+            arg: action.meta.arg.internalQueryArgs,
+          });
+        })
+        .addCase(queryThunk.fulfilled, (draft, action) => {
+          Object.assign(getQuerySubState(draft, action), {
+            status: QueryStatus.fulfilled,
+            data: action.payload,
+            resultingEntities: [
+              /* TODO */
+            ],
+          });
+        })
+        .addCase(queryThunk.rejected, (draft, action) => {
+          Object.assign(getQuerySubState(draft, action), {
+            status: QueryStatus.rejected,
+          });
+        })
+        .addCase(mutationThunk.pending, (draft, action) => {
+          Object.assign(getMutationSubState(draft, action), {
+            status: QueryStatus.pending,
+            arg: action.meta.arg.internalQueryArgs,
+          });
+        })
+        .addCase(mutationThunk.fulfilled, (draft, action) => {
+          Object.assign(getMutationSubState(draft, action), {
+            status: QueryStatus.fulfilled,
+            data: action.payload,
+            resultingEntities: [
+              /* TODO */
+            ],
+          });
+        })
+        .addCase(mutationThunk.rejected, (draft, action) => {
+          Object.assign(getMutationSubState(draft, action), {
+            status: QueryStatus.rejected,
+          });
+        });
+    },
+  });
+
+  const endpointDefinitions = endpoints({
+    query: (x) => x,
+    mutation: (x) => x,
+  });
+
+  const queryActions = Object.entries(endpointDefinitions).reduce(
+    (
+      acc,
+      [name, endpoint]: [string, EndpointDefinition<any, any, any, any>]
+    ) => {
+      if (isQueryDefinition(endpoint)) {
+        acc[name] = (arg) => {
+          const internalQueryArgs = endpoint.query(arg);
+          return queryThunk({
+            endpoint: name,
+            internalQueryArgs,
+            serializedQueryArgs: serializeQueryArgs(internalQueryArgs),
+          });
+        };
+      }
+      return acc;
+    },
+    {} as Record<string, ThunkAction<any, any, any, any>>
+  );
+
+  const mutationActions = Object.entries(endpointDefinitions).reduce(
+    (
+      acc,
+      [name, endpoint]: [string, EndpointDefinition<any, any, any, any>]
+    ) => {
+      if (isMutationDefinition(endpoint)) {
+        acc[name] = (arg) => {
+          const internalQueryArgs = endpoint.query(arg);
+          return mutationThunk({
+            endpoint: name,
+            internalQueryArgs,
+            serializedQueryArgs: serializeQueryArgs(internalQueryArgs),
+          });
+        };
+      }
+      return acc;
+    },
+    {} as Record<string, ThunkAction<any, any, any, any>>
+  );
+
+  const querySelectors = Object.entries(endpointDefinitions).reduce(
+    (acc, [name, endpoint]) => {
+      if (isQueryDefinition(endpoint)) {
+        acc[name] = (arg) => (rootState) =>
+          (rootState[reducerPath] as InternalState).queries[name]?.[
+            serializeQueryArgs(endpoint.query(arg))
+          ] ?? defaultQuerySubState;
+      }
+      return acc;
+    },
+    {} as Record<string, (arg: unknown) => (state: RootState) => unknown>
+  );
+
+  const mutationSelectors = Object.entries(endpointDefinitions).reduce(
+    (acc, [name, endpoint]) => {
+      if (isMutationDefinition(endpoint)) {
+        acc[name] = (mutationId: string) => (rootState) =>
+          (rootState[reducerPath] as InternalState).mutations[name]?.[
+            mutationId
+          ] ?? defaultMutationSubState;
+      }
+      return acc;
+    },
+    {} as Record<string, (arg: string) => (state: RootState) => unknown>
+  );
+
+  return {
+    queryActions,
+    mutationActions,
+    reducer: slice.reducer,
+    selectors: {
+      query: querySelectors,
+      mutation: mutationSelectors,
+    },
+  } as {
     queryActions: Id<QueryActions<Definitions>>;
     mutationActions: Id<MutationActions<Definitions>>;
-    getSelectors: <RootState>(
-      getSlice: (state: RootState) => QueryState
-    ) => Id<ResultSelectors<Definitions, RootState>>;
-    reducer: Reducer<QueryState, AnyAction>;
+    selectors: {
+      query: Id<QueryResultSelectors<Definitions, RootState>>;
+      mutation: Id<MutationResultSelectors<Definitions, RootState>>;
+    };
+    reducer: typeof slice.reducer;
     hooks: Hooks<Definitions>;
   };
 }
-
-interface QueryArg {
-  queryString: string;
-  method?: string;
-  body?: string;
-}
-
-interface User {
-  id: string;
-  firstName: string;
-}
-
-const api = createApi({
-  baseQuery({ queryString, method = 'GET', body }: QueryArg) {
-    return fetch(`https://example.com/${queryString}`, {
-      method,
-      body,
-    }).then(result => result.json());
-  },
-  entityTypes: ['User', 'Comment'],
-  endpoints: build => ({
-    getUser: build.query<User, string>({
-      query(id) {
-        return { queryString: `user/${id}` };
-      },
-      provides: [result => ({ type: 'User', id: result.id })],
-    }),
-    updateUser: build.mutation<User, { id: string; patch: Partial<User> }>({
-      query({ id, patch }) {
-        return {
-          queryString: `user/${id}`,
-          method: 'PATCH',
-          body: JSON.stringify(patch),
-        };
-      },
-      invalidates: [result => ({ type: 'User', id: result.id })],
-    }),
-  }),
-});
-
-const store = configureStore({
-  reducer: {
-    api: api.reducer,
-  },
-});
-type RootState = ReturnType<typeof store.getState>;
-const apiSelectors = api.getSelectors<RootState>(root => root.api);
-
-store.dispatch(api.queryActions.getUser('5'));
-
-const user5 = apiSelectors.getUser('5')(store.getState());
-
-// hooks:
-const queryResults = api.hooks.getUser.useQuery('5');
-const [runMutation, mutationResults] = api.hooks.updateUser.useMutation();
---
+++ test/example.test.ts
@@ -0,0 +1,80 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createApi } from '../src';
+
+test('example', async () => {
+  interface QueryArg {
+    queryString: string;
+    method?: string;
+    body?: string;
+  }
+
+  interface User {
+    id: string;
+    firstName: string;
+  }
+
+  const api = createApi({
+    reducerPath: 'api',
+    baseQuery({ queryString, method = 'GET', body }: QueryArg) {
+      /*
+          return fetch(`https://example.com/${queryString}`, {
+            method,
+            body,
+          }).then((result) => result.json());
+          */
+      return new Promise((resolve) =>
+        setTimeout(resolve, 500, { result: 'Hi folks!' })
+      );
+    },
+    entityTypes: ['User', 'Comment'],
+    endpoints: (build) => ({
+      getUser: build.query<User, string>({
+        query(id) {
+          return { queryString: `user/${id}` };
+        },
+        provides: [(result) => ({ type: 'User', id: result.id })],
+      }),
+      updateUser: build.mutation<User, { id: string; patch: Partial<User> }>({
+        query({ id, patch }) {
+          return {
+            queryString: `user/${id}`,
+            method: 'PATCH',
+            body: JSON.stringify(patch),
+          };
+        },
+        invalidates: [(result) => ({ type: 'User', id: result.id })],
+      }),
+    }),
+  });
+
+  const store = configureStore({
+    reducer: {
+      api: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(() => (next) => (action) => {
+        console.log(action);
+        return next(action);
+      }),
+  });
+  store.subscribe(() => {
+    console.dir(store.getState(), { depth: 5 });
+  });
+  type RootState = ReturnType<typeof store.getState>;
+  await Promise.all([
+    store.dispatch(api.queryActions.getUser('5')),
+    store.dispatch(api.queryActions.getUser('6')),
+  ]);
+
+  console.log(api.selectors.query.getUser('5')(store.getState()));
+  console.log(api.selectors.query.getUser('6')(store.getState()));
+  console.log(api.selectors.query.getUser('7')(store.getState()));
+  console.log(api.selectors.mutation.updateUser('7')(store.getState()));
+
+  /*
+
+// hooks:
+const queryResults = api.hooks.getUser.useQuery('5');
+const [runMutation, mutationResults] = api.hooks.updateUser.useMutation();
+*/
+});
