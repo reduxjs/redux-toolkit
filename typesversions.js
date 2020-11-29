0fc9e70657071da6b641d51b833f804f124d41f9
Mark Erikson <mark@isquaredsoftware.com>
2020-11-29 14:41:28 -0500

    Fix file copying issues:
    - Copy all .d.ts files in dist
    - And the utils folder itself
    - Overwrite dist/ts41Types.d.ts

--- typesversions.js
+++ typesversions.js
@@ -5,17 +5,17 @@ const { cp, mkdir, rm } = require('shelljs');
 rm('-r', 'dist/min-4.1');
 mkdir('-p', 'dist/min-4.1');
 
-const fourOneFiles = glob.sync('dist/*', { nodir: true }).filter((n) => !n.includes('min-4.1') && n.includes('.d.ts'));
-console.log(fourOneFiles);
+const fourOneFiles = glob.sync('dist/*.d.ts');
+fourOneFiles.push('dist/utils');
 
 for (let file of fourOneFiles) {
-  cp(file, 'dist/min-4.1/');
+  cp('-r', file, 'dist/min-4.1/');
 }
 
-const additionalTypes = `
+const stubTs41Types = `
 import { EndpointDefinitions } from './endpointDefinitions';
 export declare type TS41Hooks<Definitions extends EndpointDefinitions> = unknown;
 export {};
 `;
 
-fs.appendFileSync('dist/min-4.1/ts41Types.d.ts', additionalTypes);
+fs.writeFileSync('dist/ts41Types.d.ts', stubTs41Types);
