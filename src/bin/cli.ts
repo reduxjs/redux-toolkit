17ce451867a421998170765591571945b362e4b3
kahirokunn <okinakahiro@gmail.com>
2020-12-11 17:01:22 +0900

    add options for cli

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -5,16 +5,34 @@ import program = require('commander')
 
 // tslint:disable-next-line
 const meta = require('../../package.json')
-import { generateApi } from '../generate'
+import { generateApi, GenerationOptions } from '../generate'
 
 program
   .version(meta.version)
   .usage('<directory...>')
+  .option('-exportName, --exportName', 'change RTK Query Tree root name')
+  .option('-reducerPath, --reducerPath', 'pass reducer path')
+  .option('-baseQuery, --baseQuery', 'pass baseQuery name')
+  .option('-argSuffix, --argSuffix', 'pass arg suffix')
+  .option('-responseSuffix, --responseSuffix', 'pass response suffix')
   .parse(process.argv)
 
 const schemaAbsPath = path.resolve(process.cwd(), program.args[0])
+
+const options = [
+  'exportName',
+  'reducerPath',
+  'baseQuery',
+  'argSuffix',
+  'responseSuffix'
+] as const
+
 if (program.args.length !== 1) {
   program.help()
 } else {
-  generateApi(schemaAbsPath, {}).then(sourceCode => console.log(sourceCode))
+  const generateApiOptions = options.reduce((s, key) => program[key] ? ({
+    ...s,
+    [key]: program
+  }) : s, {} as GenerationOptions);
+  generateApi(schemaAbsPath, generateApiOptions).then(sourceCode => console.log(sourceCode))
 }
