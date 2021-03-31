b9fb311cc9a769b08f4bdbab6d4c80cd3801185e
Mark Erikson <mark@isquaredsoftware.com>
2021-03-30 20:38:31 -0400

    Paste in TSDX Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '.(ts|tsx)$': require.resolve('ts-jest/dist'),
+    '.(js|jsx)$': require.resolve('babel-jest') // jest's default
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'node'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+  testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx}'],
+  testURL: 'http://localhost',
+  rootDir: process.cwd(),
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname')
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [6133]
+      }
+    }
+  }
 }
