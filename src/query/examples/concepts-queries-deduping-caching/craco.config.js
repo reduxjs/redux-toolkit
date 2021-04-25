0874c526c44e1b338b656c21320b3322a7243eeb
Matt Sutkowski <msutkowski@gmail.com>
2021-04-25 11:52:00 -0700

    Add react queries example with config for review

---
+++ src/query/examples/concepts-queries-deduping-caching/craco.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const resolveFrom = require('resolve-from')
+
+module.exports = {
+  webpack: {
+    resolve: {
+      alias: {
+        '@reduxjs/toolkit/query': path.resolve(__dirname, '../../'),
+        '@reduxjs/toolkit': path.resolve(__dirname, '../../../'),
+        react: resolveFrom(
+          path.resolve(__dirname, '../../../node_modules'),
+          'react'
+        ),
+        'react-dom$': resolveFrom(
+          path.resolve(__dirname, '../../../node_modules'),
+          'react-dom'
+        ),
+        'eslint-plugin-prettier': resolveFrom(
+          'node_modules',
+          'eslint-plugin-prettier'
+        ),
+        'babel-jest': resolveFrom('node_modules', 'babel-jest'),
+      },
+    },
+  },
+}
