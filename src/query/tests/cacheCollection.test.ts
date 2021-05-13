1d4355da2d3ff173241d52493a91ec625f3b725d
Lenz Weber <mail@lenzw.de>
2021-05-13 23:51:20 +0200

    add cacheCollection tests for keepUnusedDataFor

---
+++ src/query/tests/cacheCollection.test.ts
@@ -0,0 +1,124 @@
+import { createApi } from '@reduxjs/toolkit/query'
+import { configureStore } from '@reduxjs/toolkit'
+import { fetchBaseQuery } from '../fetchBaseQuery'
+import { waitMs } from './helpers'
+import { Middleware, Reducer } from 'redux'
+
+beforeAll(() => {
+  jest.useFakeTimers()
+})
+
+const onCleanup = jest.fn()
+
+beforeEach(() => {
+  onCleanup.mockClear()
+})
+
+test(`query: await cleanup, defaults`, async () => {
+  const { store, api } = storeForApi(
+    createApi({
+      baseQuery: fetchBaseQuery({ baseUrl: 'http://example.com' }),
+      endpoints: (build) => ({
+        query: build.query<unknown, string>({
+          query: () => '/success',
+        }),
+      }),
+    })
+  )
+
+  store.dispatch(api.endpoints.query.initiate('arg')).unsubscribe()
+  jest.advanceTimersByTime(59000), await waitMs()
+  expect(onCleanup).not.toHaveBeenCalled()
+  jest.advanceTimersByTime(2000), await waitMs()
+  expect(onCleanup).toHaveBeenCalled()
+})
+
+test(`query: await cleanup, keepUnusedDataFor set`, async () => {
+  const { store, api } = storeForApi(
+    createApi({
+      baseQuery: fetchBaseQuery({ baseUrl: 'http://example.com' }),
+      endpoints: (build) => ({
+        query: build.query<unknown, string>({
+          query: () => '/success',
+        }),
+      }),
+      keepUnusedDataFor: 29,
+    })
+  )
+
+  store.dispatch(api.endpoints.query.initiate('arg')).unsubscribe()
+  jest.advanceTimersByTime(28000), await waitMs()
+  expect(onCleanup).not.toHaveBeenCalled()
+  jest.advanceTimersByTime(2000), await waitMs()
+  expect(onCleanup).toHaveBeenCalled()
+})
+
+describe(`query: await cleanup, keepUnusedDataFor set`, () => {
+  const { store, api } = storeForApi(
+    createApi({
+      baseQuery: fetchBaseQuery({ baseUrl: 'http://example.com' }),
+      endpoints: (build) => ({
+        query: build.query<unknown, string>({
+          query: () => '/success',
+        }),
+        query2: build.query<unknown, string>({
+          query: () => '/success',
+          keepUnusedDataFor: 35,
+        }),
+        query3: build.query<unknown, string>({
+          query: () => '/success',
+          keepUnusedDataFor: 0,
+        }),
+      }),
+      keepUnusedDataFor: 29,
+    })
+  )
+
+  test('global keepUnusedDataFor', async () => {
+    store.dispatch(api.endpoints.query.initiate('arg')).unsubscribe()
+    jest.advanceTimersByTime(28000), await waitMs()
+    expect(onCleanup).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(2000), await waitMs()
+    expect(onCleanup).toHaveBeenCalled()
+  })
+
+  test('endpoint keepUnusedDataFor', async () => {
+    store.dispatch(api.endpoints.query2.initiate('arg')).unsubscribe()
+    jest.advanceTimersByTime(34000), await waitMs()
+    expect(onCleanup).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(2000), await waitMs()
+    expect(onCleanup).toHaveBeenCalled()
+  })
+
+  test('endpoint keepUnusedDataFor: 0 ', async () => {
+    expect(onCleanup).not.toHaveBeenCalled()
+    store.dispatch(api.endpoints.query3.initiate('arg')).unsubscribe()
+    expect(onCleanup).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(1), await waitMs()
+    expect(onCleanup).toHaveBeenCalled()
+  })
+})
+
+function storeForApi<
+  A extends {
+    reducerPath: 'api'
+    reducer: Reducer<any, any>
+    middleware: Middleware
+    util: { resetApiState(): any }
+  }
+>(api: A) {
+  const store = configureStore({
+    reducer: { api: api.reducer },
+    middleware: (gdm) =>
+      gdm({ serializableCheck: false, immutableCheck: false }).concat(
+        api.middleware
+      ),
+  })
+  store.subscribe(() => {
+    const queryState = store.getState().api.queries
+    if (Object.keys(queryState).length === 0) {
+      onCleanup()
+    }
+  })
+  return { api, store }
+}
