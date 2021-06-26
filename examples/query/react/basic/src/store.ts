0e7a3ec0fc6fdae6f3697a8380842dcb7fe11155
Gonzo 🦻 <danigon@microsoft.com>
2021-06-25 22:16:16 -0500

    No longer need to use combineReducer since I'm not extracting store state anymore

--- examples/query/react/basic/src/store.ts
+++ examples/query/react/basic/src/store.ts
@@ -1,13 +1,11 @@
-import { combineReducers, configureStore, Store } from '@reduxjs/toolkit'
+import { configureStore, Store } from '@reduxjs/toolkit'
 import { pokemonApi } from './services/pokemon'
-
-const reducer = combineReducers({
-  [pokemonApi.reducerPath]: pokemonApi.reducer,
-})
 
 export const createStore = (): Store => {
   return configureStore({
-    reducer: reducer,
+    reducer: {
+      [pokemonApi.reducerPath]: pokemonApi.reducer,
+    },
     // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(pokemonApi.middleware),
