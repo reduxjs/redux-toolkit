985d8dfb1eb403fb914d901e95793b32bd64a7ee
Lenz Weber <mail@lenzw.de>
2021-07-13 22:20:38 +0200

    re-add openapi-types

--- src/utils/getOperationDefinitions.ts
+++ src/utils/getOperationDefinitions.ts
@@ -3,15 +3,17 @@ import { OperationDefinition, operationKeys } from '../types';
 
 export function getOperationDefinitions(v3Doc: OpenAPIV3.Document): OperationDefinition[] {
   return Object.entries(v3Doc.paths).flatMap(([path, pathItem]) =>
-    Object.entries(pathItem)
-      .filter((arg): arg is [typeof operationKeys[number], OpenAPIV3.OperationObject] =>
-        operationKeys.includes(arg[0] as any)
-      )
-      .map(([verb, operation]) => ({
-        path,
-        verb,
-        pathItem,
-        operation,
-      }))
+    !pathItem
+      ? []
+      : Object.entries(pathItem)
+          .filter((arg): arg is [typeof operationKeys[number], OpenAPIV3.OperationObject] =>
+            operationKeys.includes(arg[0] as any)
+          )
+          .map(([verb, operation]) => ({
+            path,
+            verb,
+            pathItem,
+            operation,
+          }))
   );
 }
