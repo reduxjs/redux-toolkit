468f72a4a63b2ef3689592109d62c633f5579667
Matt Sutkowski <msutkowski@gmail.com>
2021-09-21 09:31:45 -0700

    Return a skipToken if it's a skipToken instead of serialized values

--- packages/toolkit/src/query/react/useSerializedStableValue.ts
+++ packages/toolkit/src/query/react/useSerializedStableValue.ts
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useMemo } from 'react'
 import type { SerializeQueryArgs } from '@reduxjs/toolkit/dist/query/defaultSerializeQueryArgs'
 import type { EndpointDefinition } from '@reduxjs/toolkit/dist/query/endpointDefinitions'
+import { skipToken } from '@reduxjs/toolkit/query'
 
 export function useStableQueryArgs<T>(
-  queryArgs: T,
+  queryArgs: T | typeof skipToken,
   serialize: SerializeQueryArgs<any>,
   endpointDefinition: EndpointDefinition<any, any, any, any>,
   endpointName: string
@@ -11,7 +12,10 @@ export function useStableQueryArgs<T>(
   const incoming = useMemo(
     () => ({
       queryArgs,
-      serialized: serialize({ queryArgs, endpointDefinition, endpointName }),
+      serialized:
+        queryArgs === skipToken
+          ? skipToken
+          : serialize({ queryArgs, endpointDefinition, endpointName }),
     }),
     [queryArgs, serialize, endpointDefinition, endpointName]
   )
