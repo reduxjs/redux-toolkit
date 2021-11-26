215b3aca70404dedfd60e71e4cfd206e3646c8a9
Lenz Weber <lenz.weber@mayflower.de>
2021-11-26 14:50:05 +0100

    validate TS version compatibility

--- packages/rtk-query-codegen-openapi/src/index.ts
+++ packages/rtk-query-codegen-openapi/src/index.ts
@@ -1,11 +1,14 @@
 import path from 'path';
 import fs from 'fs';
+import ts from 'typescript';
 import type { CommonOptions, ConfigFile, GenerationOptions, OutputFileOptions } from './types';
 import { generateApi } from './generate';
 import { isValidUrl, prettify } from './utils';
 export { ConfigFile } from './types';
 
 export async function generateEndpoints(options: GenerationOptions): Promise<string | void> {
+  assertCompatibleTsVersions();
+
   const schemaLocation = options.schemaFile;
 
   const schemaAbsPath = isValidUrl(options.schemaFile)
@@ -37,4 +40,16 @@ export function parseConfig(fullConfig: ConfigFile) {
     outFiles.push(fullConfig);
   }
   return outFiles;
+}
+
+function assertCompatibleTsVersions() {
+  const oazapftsTsVersion = require(require.resolve('typescript', { paths: [require.resolve('oazapfts')] })).version;
+  if (oazapftsTsVersion !== ts.version) {
+    throw new Error(
+      `TypeScript versions available to the \`@rtk-query/codegen-openapi\` package (${ts.version}) and the \`oazapfts\` package (${oazapftsTsVersion}) differ.
+      This makes code generation impossible.
+      This is a problem with your local node_modules installation and can be resolved with a "resolutions" field in your package.json if you are using  \`yarn\`.
+      Otherwise, use a tool like \`override-require\` to ensure all imports of TypeScript will result in the same package version.`
+    );
+  }
 }
