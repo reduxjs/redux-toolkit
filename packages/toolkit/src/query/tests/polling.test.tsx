e99690d71ad0e4a9ec93ef2203f60b4d9d2dc88e
Daniel Breen <daniel.breen@kno2.com>
2022-01-17 22:48:01 -0600

    Start writing tests

---
+++ packages/toolkit/src/query/tests/polling.test.tsx
@@ -0,0 +1,37 @@
+import { createApi } from '@reduxjs/toolkit/query'
+import { actionsReducer, setupApiStore, waitMs } from './helpers'
+
+const baseQuery = (args?: any) => ({ data: args })
+const api = createApi({
+  baseQuery,
+  tagTypes: ['Posts'],
+  endpoints: (build) => ({
+    getPosts: build.query<unknown, number>({
+      query(pageNumber) {
+        return { url: 'posts', params: pageNumber }
+      },
+      providesTags: ['Posts'],
+    })
+  }),
+})
+const { getPosts } = api.endpoints
+
+const storeRef = setupApiStore(api, {
+  ...actionsReducer,
+})
+
+describe('polling tests', () => {
+  test('resetApiState clears intervals', async () => {
+    await storeRef.store.dispatch(
+      getPosts.initiate(1, { subscriptionOptions: { pollingInterval: 1000 } })
+    )
+
+    storeRef.store.dispatch(api.util.resetApiState())
+
+    const { subscriptions } = storeRef.store.getState().api
+    const key = 'getPosts(1)'
+    expect(subscriptions).toHaveProperty(key) // TODO: key is gone after resetApiState. Expected?
+    expect(subscriptions[key]?.pollingInterval).toBeFalsy()
+  })
+})
+
