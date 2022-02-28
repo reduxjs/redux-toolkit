0220bfc77acabfc4a92e39742507d533fd52149b
Todd Brown <todd.brown@hey.com>
2022-02-28 21:01:33 +0000

    feat: respect prefers dark mode

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -69,6 +69,9 @@ module.exports = {
       theme: require('./src/js/monokaiTheme.js'),
     },
     image: 'img/redux-logo-landscape.png',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: 'Redux Toolkit',
       logo: {
