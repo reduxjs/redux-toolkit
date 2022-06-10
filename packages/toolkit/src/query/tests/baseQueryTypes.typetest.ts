77ab13aeadd07b6b95af4e157916d6000daccbfb
Tamás Szigeti <tamas@szigeti.one>
2022-06-10 20:37:29 +0200

    Run prettier

--- packages/toolkit/src/query/tests/baseQueryTypes.typetest.ts
+++ packages/toolkit/src/query/tests/baseQueryTypes.typetest.ts
@@ -1,39 +1,33 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query'
 
 /**
  * Test: BaseQuery propagates meta types to its callbacks
  */
 {
-    createApi({
-        baseQuery: fetchBaseQuery(),
-        endpoints: (build) => ({
-            getDummy: build.query<null, undefined>({
-                query: () => "dummy",
-                onCacheEntryAdded: async (
-                    arg,
-                    { cacheDataLoaded }
-                ) => {
-                    const { meta } = await cacheDataLoaded;
-                    const { request, response } = meta!; // We expect request and response to be there
-                },
-            })
-        })
-    })
+  createApi({
+    baseQuery: fetchBaseQuery(),
+    endpoints: (build) => ({
+      getDummy: build.query<null, undefined>({
+        query: () => 'dummy',
+        onCacheEntryAdded: async (arg, { cacheDataLoaded }) => {
+          const { meta } = await cacheDataLoaded
+          const { request, response } = meta! // We expect request and response to be there
+        },
+      }),
+    }),
+  })
 
-    const baseQuery = retry(fetchBaseQuery()); // Expect meta type even when wrapped with retry
-    createApi({
-        baseQuery,
-        endpoints: (build) => ({
-            getDummy: build.query<null, undefined>({
-                query: () => "dummy",
-                onCacheEntryAdded: async (
-                    arg,
-                    { cacheDataLoaded }
-                ) => {
-                    const { meta } = await cacheDataLoaded;
-                    const { request, response } = meta!;
-                },
-            })
-        })
-    })
+  const baseQuery = retry(fetchBaseQuery()) // Expect meta type even when wrapped with retry
+  createApi({
+    baseQuery,
+    endpoints: (build) => ({
+      getDummy: build.query<null, undefined>({
+        query: () => 'dummy',
+        onCacheEntryAdded: async (arg, { cacheDataLoaded }) => {
+          const { meta } = await cacheDataLoaded
+          const { request, response } = meta!
+        },
+      }),
+    }),
+  })
 }
