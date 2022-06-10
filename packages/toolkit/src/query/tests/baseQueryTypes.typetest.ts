6e94b0e36c7fbdf1d9f90855ec6f9d6cace467f6
Tamás Szigeti <tamas@szigeti.one>
2022-06-10 20:18:14 +0200

    Add type test for meta

---
+++ packages/toolkit/src/query/tests/baseQueryTypes.typetest.ts
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query";
+
+/**
+ * Test: BaseQuery propagates meta types to its callbacks
+ */
+{
+    createApi({
+        baseQuery: fetchBaseQuery(),
+        endpoints: (build) => ({
+            getDummy: build.query<null, undefined>({
+                query: () => "dummy",
+                onCacheEntryAdded: async (
+                    arg,
+                    { cacheDataLoaded }
+                ) => {
+                    const { meta } = await cacheDataLoaded;
+                    const { request, response } = meta!; // We expect request and response to be there
+                },
+            })
+        })
+    })
+
+    const baseQuery = retry(fetchBaseQuery()); // Expect meta type even when wrapped with retry
+    createApi({
+        baseQuery,
+        endpoints: (build) => ({
+            getDummy: build.query<null, undefined>({
+                query: () => "dummy",
+                onCacheEntryAdded: async (
+                    arg,
+                    { cacheDataLoaded }
+                ) => {
+                    const { meta } = await cacheDataLoaded;
+                    const { request, response } = meta!;
+                },
+            })
+        })
+    })
+}
