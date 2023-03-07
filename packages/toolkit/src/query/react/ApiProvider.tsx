2b30ed22af45166c0ae8fd61ff608e35f79e730e
Harris Miller <hmiller@alteryx.com>
2023-03-07 12:40:22 -0700

    add in createApiProvider

--- packages/toolkit/src/query/react/ApiProvider.tsx
+++ packages/toolkit/src/query/react/ApiProvider.tsx
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit'
+import type { Action, AnyAction, Middleware, StoreEnhancer } from 'redux'
 import type { Context } from 'react'
 import { useEffect } from 'react'
 import React from 'react'
@@ -6,57 +6,77 @@ import type { ReactReduxContextValue } from 'react-redux'
 import { Provider } from 'react-redux'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import type { Api } from '@reduxjs/toolkit/dist/query/apiTypes'
+import type { ConfigureStoreOptions } from '../../configureStore';
+import { configureStore } from '../../configureStore'
+import type { ThunkMiddlewareFor } from '../../getDefaultMiddleware'
+
+// copied from configureStore
+type Middlewares<S> = ReadonlyArray<Middleware<{}, S>>
+
+type Enhancers = ReadonlyArray<StoreEnhancer>
+
+export function createApiProvider<
+  A extends Action = AnyAction,
+  M extends Middlewares<any> = [ThunkMiddlewareFor<any>],
+  E extends Enhancers = [StoreEnhancer]
+>(options: Omit<ConfigureStoreOptions<any, A, M, E>, 'reducer'>) {
+  const { middleware, ...remaining } = options;
+
+  return function <A extends Api<any, {}, any, any>>(props: {
+    children: any
+    api: A
+    setupListeners?: Parameters<typeof setupListeners>[1] | false
+    context?: Context<ReactReduxContextValue>
+  }) {
+    const [store] = React.useState(() =>
+      configureStore({
+        reducer: {
+          [props.api.reducerPath]: props.api.reducer,
+        },
+        middleware: (gDM) => gDM().concat(props.api.middleware),
+        ...remaining
+      })
+    )
+    // Adds the event listeners for online/offline/focus/etc
+    useEffect(
+      (): undefined | (() => void) =>
+        props.setupListeners === false
+          ? undefined
+          : setupListeners(store.dispatch, props.setupListeners),
+      [props.setupListeners, store.dispatch]
+    )
+
+    return (
+      <Provider store={store} context={props.context}>
+        {props.children}
+      </Provider>
+    )
+  }
+}
 
 /**
- * Can be used as a `Provider` if you **do not already have a Redux store**.
- *
- * @example
- * ```tsx
- * // codeblock-meta title="Basic usage - wrap your App with ApiProvider"
- * import * as React from 'react';
- * import { ApiProvider } from '@reduxjs/toolkit/query/react';
- * import { Pokemon } from './features/Pokemon';
- *
- * function App() {
- *   return (
- *     <ApiProvider api={api}>
- *       <Pokemon />
- *     </ApiProvider>
- *   );
- * }
- * ```
- *
- * @remarks
- * Using this together with an existing redux store, both will
- * conflict with each other - please use the traditional redux setup
- * in that case.
- */
-export function ApiProvider<A extends Api<any, {}, any, any>>(props: {
-  children: any
-  api: A
-  setupListeners?: Parameters<typeof setupListeners>[1] | false
-  context?: Context<ReactReduxContextValue>
-}) {
-  const [store] = React.useState(() =>
-    configureStore({
-      reducer: {
-        [props.api.reducerPath]: props.api.reducer,
-      },
-      middleware: (gDM) => gDM().concat(props.api.middleware),
-    })
-  )
-  // Adds the event listeners for online/offline/focus/etc
-  useEffect(
-    (): undefined | (() => void) =>
-      props.setupListeners === false
-        ? undefined
-        : setupListeners(store.dispatch, props.setupListeners),
-    [props.setupListeners, store.dispatch]
-  )
+   * Can be used as a `Provider` if you **do not already have a Redux store**.
+   *
+   * @example
+   * ```tsx
+   * // codeblock-meta title="Basic usage - wrap your App with ApiProvider"
+   * import * as React from 'react';
+   * import { ApiProvider } from '@reduxjs/toolkit/query/react';
+   * import { Pokemon } from './features/Pokemon';
+   *
+   * function App() {
+   *   return (
+   *     <ApiProvider api={api}>
+   *       <Pokemon />
+   *     </ApiProvider>
+   *   );
+   * }
+   * ```
+   *
+   * @remarks
+   * Using this together with an existing redux store, both will
+   * conflict with each other - please use the traditional redux setup
+   * in that case.
+   */
+export const ApiProvider = createApiProvider({});
 
-  return (
-    <Provider store={store} context={props.context}>
-      {props.children}
-    </Provider>
-  )
-}
