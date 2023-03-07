7fe1f78d06514fc5c56b7291aad4b00206055670
Harris Miller <hmiller@alteryx.com>
2023-03-07 13:05:07 -0700

    handle middleware correctly

--- packages/toolkit/src/query/react/ApiProvider.tsx
+++ packages/toolkit/src/query/react/ApiProvider.tsx
@@ -8,19 +8,27 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import type { Api } from '@reduxjs/toolkit/dist/query/apiTypes'
 import type { ConfigureStoreOptions } from '../../configureStore';
 import { configureStore } from '../../configureStore'
-import type { ThunkMiddlewareFor } from '../../getDefaultMiddleware'
+import type { CurriedGetDefaultMiddleware, ThunkMiddlewareFor } from '../../getDefaultMiddleware'
 
 // copied from configureStore
 type Middlewares<S> = ReadonlyArray<Middleware<{}, S>>
 
 type Enhancers = ReadonlyArray<StoreEnhancer>
 
+/**
+ * Low-level way to create a custom ApiProvider with configureStore options
+ * `options` is the same as for `configureStore` minus the `reducer` property
+ * Useful for if you need to set devtool or additional middlewares to the store
+ */
 export function createApiProvider<
   A extends Action = AnyAction,
   M extends Middlewares<any> = [ThunkMiddlewareFor<any>],
   E extends Enhancers = [StoreEnhancer]
 >(options: Omit<ConfigureStoreOptions<any, A, M, E>, 'reducer'>) {
   const { middleware, ...remaining } = options;
+
+  const processMiddleware = (apiMiddleware: Middleware) => (gDM: CurriedGetDefaultMiddleware<any>) =>
+    Array.isArray(middleware) ? middleware.concat([apiMiddleware]) : gDM().concat(apiMiddleware);
 
   return function <A extends Api<any, {}, any, any>>(props: {
     children: any
@@ -33,7 +41,7 @@ export function createApiProvider<
         reducer: {
           [props.api.reducerPath]: props.api.reducer,
         },
-        middleware: (gDM) => gDM().concat(props.api.middleware),
+        middleware: processMiddleware(props.api.middleware),
         ...remaining
       })
     )
@@ -55,28 +63,28 @@ export function createApiProvider<
 }
 
 /**
-   * Can be used as a `Provider` if you **do not already have a Redux store**.
-   *
-   * @example
-   * ```tsx
-   * // codeblock-meta title="Basic usage - wrap your App with ApiProvider"
-   * import * as React from 'react';
-   * import { ApiProvider } from '@reduxjs/toolkit/query/react';
-   * import { Pokemon } from './features/Pokemon';
-   *
-   * function App() {
-   *   return (
-   *     <ApiProvider api={api}>
-   *       <Pokemon />
-   *     </ApiProvider>
-   *   );
-   * }
-   * ```
-   *
-   * @remarks
-   * Using this together with an existing redux store, both will
-   * conflict with each other - please use the traditional redux setup
-   * in that case.
-   */
+ * Can be used as a `Provider` if you **do not already have a Redux store**.
+ *
+ * @example
+ * ```tsx
+ * // codeblock-meta title="Basic usage - wrap your App with ApiProvider"
+ * import * as React from 'react';
+ * import { ApiProvider } from '@reduxjs/toolkit/query/react';
+ * import { Pokemon } from './features/Pokemon';
+ *
+ * function App() {
+ *   return (
+ *     <ApiProvider api={api}>
+ *       <Pokemon />
+ *     </ApiProvider>
+ *   );
+ * }
+ * ```
+ *
+ * @remarks
+ * Using this together with an existing redux store, both will
+ * conflict with each other - please use the traditional redux setup
+ * in that case.
+ */
 export const ApiProvider = createApiProvider({});
 
