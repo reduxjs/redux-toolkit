293a9b9a1df9ae4c027e4251f3f0847a9305411b
ben.durrant <ben.durrant@actual-experience.com>
2023-04-04 23:55:07 +0100

    make configurable createEntityAdapter

--- packages/toolkit/src/entities/state_selectors.ts
+++ packages/toolkit/src/entities/state_selectors.ts
@@ -1,5 +1,6 @@
 import type { Selector } from 'reselect'
-import { createDraftSafeSelector } from '../createDraftSafeSelector'
+import type { BuildCreateDraftSafeSelectorConfiguration } from '../createDraftSafeSelector'
+import { buildCreateDraftSafeSelector } from '../createDraftSafeSelector'
 import type {
   EntityState,
   EntitySelectors,
@@ -7,61 +8,69 @@ import type {
   EntityId,
 } from './models'
 
-export function createSelectorsFactory<T>() {
-  function getSelectors(): EntitySelectors<T, EntityState<T>>
-  function getSelectors<V>(
-    selectState: (state: V) => EntityState<T>
-  ): EntitySelectors<T, V>
-  function getSelectors<V>(
-    selectState?: (state: V) => EntityState<T>
-  ): EntitySelectors<T, any> {
-    const selectIds = (state: EntityState<T>) => state.ids
+export function buildCreateSelectorsFactory(
+  config: BuildCreateDraftSafeSelectorConfiguration
+) {
+  return function createSelectorsFactory<T>() {
+    function getSelectors(): EntitySelectors<T, EntityState<T>>
+    function getSelectors<V>(
+      selectState: (state: V) => EntityState<T>
+    ): EntitySelectors<T, V>
+    function getSelectors<V>(
+      selectState?: (state: V) => EntityState<T>
+    ): EntitySelectors<T, any> {
+      const createDraftSafeSelector = buildCreateDraftSafeSelector(config)
+      const selectIds = (state: EntityState<T>) => state.ids
 
-    const selectEntities = (state: EntityState<T>) => state.entities
+      const selectEntities = (state: EntityState<T>) => state.entities
 
-    const selectAll = createDraftSafeSelector(
-      selectIds,
-      selectEntities,
-      (ids, entities): T[] => ids.map((id) => entities[id]!)
-    )
-
-    const selectId = (_: unknown, id: EntityId) => id
-
-    const selectById = (entities: Dictionary<T>, id: EntityId) => entities[id]
-
-    const selectTotal = createDraftSafeSelector(selectIds, (ids) => ids.length)
-
-    if (!selectState) {
-      return {
+      const selectAll = createDraftSafeSelector(
         selectIds,
         selectEntities,
-        selectAll,
-        selectTotal,
-        selectById: createDraftSafeSelector(
+        (ids, entities): T[] => ids.map((id) => entities[id]!)
+      )
+
+      const selectId = (_: unknown, id: EntityId) => id
+
+      const selectById = (entities: Dictionary<T>, id: EntityId) => entities[id]
+
+      const selectTotal = createDraftSafeSelector(
+        selectIds,
+        (ids) => ids.length
+      )
+
+      if (!selectState) {
+        return {
+          selectIds,
           selectEntities,
+          selectAll,
+          selectTotal,
+          selectById: createDraftSafeSelector(
+            selectEntities,
+            selectId,
+            selectById
+          ),
+        }
+      }
+
+      const selectGlobalizedEntities = createDraftSafeSelector(
+        selectState as Selector<V, EntityState<T>>,
+        selectEntities
+      )
+
+      return {
+        selectIds: createDraftSafeSelector(selectState, selectIds),
+        selectEntities: selectGlobalizedEntities,
+        selectAll: createDraftSafeSelector(selectState, selectAll),
+        selectTotal: createDraftSafeSelector(selectState, selectTotal),
+        selectById: createDraftSafeSelector(
+          selectGlobalizedEntities,
           selectId,
           selectById
         ),
       }
     }
 
-    const selectGlobalizedEntities = createDraftSafeSelector(
-      selectState as Selector<V, EntityState<T>>,
-      selectEntities
-    )
-
-    return {
-      selectIds: createDraftSafeSelector(selectState, selectIds),
-      selectEntities: selectGlobalizedEntities,
-      selectAll: createDraftSafeSelector(selectState, selectAll),
-      selectTotal: createDraftSafeSelector(selectState, selectTotal),
-      selectById: createDraftSafeSelector(
-        selectGlobalizedEntities,
-        selectId,
-        selectById
-      ),
-    }
+    return { getSelectors }
   }
-
-  return { getSelectors }
 }
--- packages/toolkit/src/entities/unsorted_state_adapter.ts
+++ packages/toolkit/src/entities/unsorted_state_adapter.ts
@@ -5,9 +5,10 @@ import type {
   Update,
   EntityId,
 } from './models'
+import type { BuildStateOperatorConfiguration } from './state_adapter'
 import {
-  createStateOperator,
-  createSingleArgumentStateOperator,
+  buildCreateSingleArgumentStateOperator,
+  buildCreateStateOperator,
 } from './state_adapter'
 import {
   selectIdValue,
@@ -15,184 +16,191 @@ import {
   splitAddedUpdatedEntities,
 } from './utils'
 
-export function createUnsortedStateAdapter<T>(
-  selectId: IdSelector<T>
-): EntityStateAdapter<T> {
-  type R = EntityState<T>
+export function buildCreateUnsortedStateAdapter(
+  config: BuildStateOperatorConfiguration
+) {
+  const createSingleArgumentStateOperator =
+    buildCreateSingleArgumentStateOperator(config)
+  const createStateOperator = buildCreateStateOperator(config)
+  return function createUnsortedStateAdapter<T>(
+    selectId: IdSelector<T>
+  ): EntityStateAdapter<T> {
+    type R = EntityState<T>
 
-  function addOneMutably(entity: T, state: R): void {
-    const key = selectIdValue(entity, selectId)
+    function addOneMutably(entity: T, state: R): void {
+      const key = selectIdValue(entity, selectId)
 
-    if (key in state.entities) {
-      return
-    }
-
-    state.ids.push(key)
-    state.entities[key] = entity
-  }
-
-  function addManyMutably(
-    newEntities: readonly T[] | Record<EntityId, T>,
-    state: R
-  ): void {
-    newEntities = ensureEntitiesArray(newEntities)
-
-    for (const entity of newEntities) {
-      addOneMutably(entity, state)
-    }
-  }
-
-  function setOneMutably(entity: T, state: R): void {
-    const key = selectIdValue(entity, selectId)
-    if (!(key in state.entities)) {
-      state.ids.push(key)
-    }
-    state.entities[key] = entity
-  }
-
-  function setManyMutably(
-    newEntities: readonly T[] | Record<EntityId, T>,
-    state: R
-  ): void {
-    newEntities = ensureEntitiesArray(newEntities)
-    for (const entity of newEntities) {
-      setOneMutably(entity, state)
-    }
-  }
-
-  function setAllMutably(
-    newEntities: readonly T[] | Record<EntityId, T>,
-    state: R
-  ): void {
-    newEntities = ensureEntitiesArray(newEntities)
-
-    state.ids = []
-    state.entities = {}
-
-    addManyMutably(newEntities, state)
-  }
-
-  function removeOneMutably(key: EntityId, state: R): void {
-    return removeManyMutably([key], state)
-  }
-
-  function removeManyMutably(keys: readonly EntityId[], state: R): void {
-    let didMutate = false
-
-    keys.forEach((key) => {
       if (key in state.entities) {
-        delete state.entities[key]
-        didMutate = true
+        return
       }
-    })
 
-    if (didMutate) {
-      state.ids = state.ids.filter((id) => id in state.entities)
-    }
-  }
-
-  function removeAllMutably(state: R): void {
-    Object.assign(state, {
-      ids: [],
-      entities: {},
-    })
-  }
-
-  function takeNewKey(
-    keys: { [id: string]: EntityId },
-    update: Update<T>,
-    state: R
-  ): boolean {
-    const original = state.entities[update.id]
-    const updated: T = Object.assign({}, original, update.changes)
-    const newKey = selectIdValue(updated, selectId)
-    const hasNewKey = newKey !== update.id
-
-    if (hasNewKey) {
-      keys[update.id] = newKey
-      delete state.entities[update.id]
+      state.ids.push(key)
+      state.entities[key] = entity
     }
 
-    state.entities[newKey] = updated
+    function addManyMutably(
+      newEntities: readonly T[] | Record<EntityId, T>,
+      state: R
+    ): void {
+      newEntities = ensureEntitiesArray(newEntities)
 
-    return hasNewKey
-  }
+      for (const entity of newEntities) {
+        addOneMutably(entity, state)
+      }
+    }
 
-  function updateOneMutably(update: Update<T>, state: R): void {
-    return updateManyMutably([update], state)
-  }
+    function setOneMutably(entity: T, state: R): void {
+      const key = selectIdValue(entity, selectId)
+      if (!(key in state.entities)) {
+        state.ids.push(key)
+      }
+      state.entities[key] = entity
+    }
 
-  function updateManyMutably(
-    updates: ReadonlyArray<Update<T>>,
-    state: R
-  ): void {
-    const newKeys: { [id: string]: EntityId } = {}
+    function setManyMutably(
+      newEntities: readonly T[] | Record<EntityId, T>,
+      state: R
+    ): void {
+      newEntities = ensureEntitiesArray(newEntities)
+      for (const entity of newEntities) {
+        setOneMutably(entity, state)
+      }
+    }
 
-    const updatesPerEntity: { [id: string]: Update<T> } = {}
+    function setAllMutably(
+      newEntities: readonly T[] | Record<EntityId, T>,
+      state: R
+    ): void {
+      newEntities = ensureEntitiesArray(newEntities)
 
-    updates.forEach((update) => {
-      // Only apply updates to entities that currently exist
-      if (update.id in state.entities) {
-        // If there are multiple updates to one entity, merge them together
-        updatesPerEntity[update.id] = {
-          id: update.id,
-          // Spreads ignore falsy values, so this works even if there isn't
-          // an existing update already at this key
-          changes: {
-            ...(updatesPerEntity[update.id]
-              ? updatesPerEntity[update.id].changes
-              : null),
-            ...update.changes,
-          },
+      state.ids = []
+      state.entities = {}
+
+      addManyMutably(newEntities, state)
+    }
+
+    function removeOneMutably(key: EntityId, state: R): void {
+      return removeManyMutably([key], state)
+    }
+
+    function removeManyMutably(keys: readonly EntityId[], state: R): void {
+      let didMutate = false
+
+      keys.forEach((key) => {
+        if (key in state.entities) {
+          delete state.entities[key]
+          didMutate = true
+        }
+      })
+
+      if (didMutate) {
+        state.ids = state.ids.filter((id) => id in state.entities)
+      }
+    }
+
+    function removeAllMutably(state: R): void {
+      Object.assign(state, {
+        ids: [],
+        entities: {},
+      })
+    }
+
+    function takeNewKey(
+      keys: { [id: string]: EntityId },
+      update: Update<T>,
+      state: R
+    ): boolean {
+      const original = state.entities[update.id]
+      const updated: T = Object.assign({}, original, update.changes)
+      const newKey = selectIdValue(updated, selectId)
+      const hasNewKey = newKey !== update.id
+
+      if (hasNewKey) {
+        keys[update.id] = newKey
+        delete state.entities[update.id]
+      }
+
+      state.entities[newKey] = updated
+
+      return hasNewKey
+    }
+
+    function updateOneMutably(update: Update<T>, state: R): void {
+      return updateManyMutably([update], state)
+    }
+
+    function updateManyMutably(
+      updates: ReadonlyArray<Update<T>>,
+      state: R
+    ): void {
+      const newKeys: { [id: string]: EntityId } = {}
+
+      const updatesPerEntity: { [id: string]: Update<T> } = {}
+
+      updates.forEach((update) => {
+        // Only apply updates to entities that currently exist
+        if (update.id in state.entities) {
+          // If there are multiple updates to one entity, merge them together
+          updatesPerEntity[update.id] = {
+            id: update.id,
+            // Spreads ignore falsy values, so this works even if there isn't
+            // an existing update already at this key
+            changes: {
+              ...(updatesPerEntity[update.id]
+                ? updatesPerEntity[update.id].changes
+                : null),
+              ...update.changes,
+            },
+          }
+        }
+      })
+
+      updates = Object.values(updatesPerEntity)
+
+      const didMutateEntities = updates.length > 0
+
+      if (didMutateEntities) {
+        const didMutateIds =
+          updates.filter((update) => takeNewKey(newKeys, update, state))
+            .length > 0
+
+        if (didMutateIds) {
+          state.ids = Object.keys(state.entities)
         }
       }
-    })
-
-    updates = Object.values(updatesPerEntity)
-
-    const didMutateEntities = updates.length > 0
-
-    if (didMutateEntities) {
-      const didMutateIds =
-        updates.filter((update) => takeNewKey(newKeys, update, state)).length >
-        0
-
-      if (didMutateIds) {
-        state.ids = Object.keys(state.entities)
-      }
     }
-  }
 
-  function upsertOneMutably(entity: T, state: R): void {
-    return upsertManyMutably([entity], state)
-  }
+    function upsertOneMutably(entity: T, state: R): void {
+      return upsertManyMutably([entity], state)
+    }
 
-  function upsertManyMutably(
-    newEntities: readonly T[] | Record<EntityId, T>,
-    state: R
-  ): void {
-    const [added, updated] = splitAddedUpdatedEntities<T>(
-      newEntities,
-      selectId,
-      state
-    )
+    function upsertManyMutably(
+      newEntities: readonly T[] | Record<EntityId, T>,
+      state: R
+    ): void {
+      const [added, updated] = splitAddedUpdatedEntities<T>(
+        newEntities,
+        selectId,
+        state
+      )
 
-    updateManyMutably(updated, state)
-    addManyMutably(added, state)
-  }
+      updateManyMutably(updated, state)
+      addManyMutably(added, state)
+    }
 
-  return {
-    removeAll: createSingleArgumentStateOperator(removeAllMutably),
-    addOne: createStateOperator(addOneMutably),
-    addMany: createStateOperator(addManyMutably),
-    setOne: createStateOperator(setOneMutably),
-    setMany: createStateOperator(setManyMutably),
-    setAll: createStateOperator(setAllMutably),
-    updateOne: createStateOperator(updateOneMutably),
-    updateMany: createStateOperator(updateManyMutably),
-    upsertOne: createStateOperator(upsertOneMutably),
-    upsertMany: createStateOperator(upsertManyMutably),
-    removeOne: createStateOperator(removeOneMutably),
-    removeMany: createStateOperator(removeManyMutably),
+    return {
+      removeAll: createSingleArgumentStateOperator(removeAllMutably),
+      addOne: createStateOperator(addOneMutably),
+      addMany: createStateOperator(addManyMutably),
+      setOne: createStateOperator(setOneMutably),
+      setMany: createStateOperator(setManyMutably),
+      setAll: createStateOperator(setAllMutably),
+      updateOne: createStateOperator(updateOneMutably),
+      updateMany: createStateOperator(updateManyMutably),
+      upsertOne: createStateOperator(upsertOneMutably),
+      upsertMany: createStateOperator(upsertManyMutably),
+      removeOne: createStateOperator(removeOneMutably),
+      removeMany: createStateOperator(removeManyMutably),
+    }
   }
 }
