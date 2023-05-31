a5bd1e6065c5ebdddd78305edce21b5569b4c2ea
Evert Bouw <evert@smanwaarnemingen.nl>
2023-05-31 11:06:51 +0200

    fix syntax for users with noPropertyAccessFromIndexSignature enabled

--- packages/toolkit/src/uncheckedindexed.ts
+++ packages/toolkit/src/uncheckedindexed.ts
@@ -2,7 +2,7 @@
 // relies on remaining as a TS file, not .d.ts
 type IfMaybeUndefined<T, True, False> = [undefined] extends [T] ? True : False
 
-const testAccess = ({} as Record<string, 0>).a
+const testAccess = ({} as Record<string, 0>)['a']
 
 export type IfUncheckedIndexedAccess<True, False> = IfMaybeUndefined<
   typeof testAccess,
