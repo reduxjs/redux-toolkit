25d7c36c12e5c8ae14ffa03f9322620e18429c54
Arya Emami <aryaemami59@yahoo.com>
2024-01-05 17:20:36 -0600

    Fix formatting issue in `vitest.config.ts`

--- packages/toolkit/vitest.config.ts
+++ packages/toolkit/vitest.config.ts
@@ -14,10 +14,7 @@ export default defineConfig({
     setupFiles: ['./vitest.setup.js'],
     include: ['./src/**/*.(spec|test).[jt]s?(x)'],
     alias: {
-      '@reduxjs/toolkit/query/react': path.join(
-        __dirname,
-        './src/query/react/index.ts'
-      ), // @remap-prod-remove-line
+      '@reduxjs/toolkit/query/react': path.join(__dirname,'./src/query/react/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit/query': path.join(__dirname, './src/query/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit/react': path.join(__dirname, './src/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit': path.join(__dirname, './src/index.ts'), // @remap-prod-remove-line
