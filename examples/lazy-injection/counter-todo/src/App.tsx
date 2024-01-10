57a00afd19d36a671c7f6e079bf2fca53886da9f
Ben Durrant <ben.j.durrant@gmail.com>
2024-01-10 23:04:55 +0000

    clone vite example

---
+++ examples/lazy-injection/counter-todo/src/App.tsx
@@ -0,0 +1,68 @@
+import "./App.css"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Quotes />
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://reselect.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Reselect
+          </a>
+        </span>
+      </header>
+    </div>
+  )
+}
+
+export default App
