1e4363c3c2e8cb74494b4d16a8227950593c039c
Ben Durrant <ben.durrant@marketdojo.com>
2024-01-12 14:23:59 +0000

    remove open: true

--- examples/lazy-injection/counter-todo/vite.config.ts
+++ examples/lazy-injection/counter-todo/vite.config.ts
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
   test: {
     globals: true,
     environment: "jsdom",
