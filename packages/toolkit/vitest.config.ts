763a598a4acdf2abbbec8981cf67dd97cb1a5ed3
Arya Emami <aryaemami59@yahoo.com>
2024-01-12 08:35:16 -0600

    Prevent Prettier from wrapping lines in `vitest.config.ts`

--- packages/toolkit/vitest.config.ts
+++ packages/toolkit/vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config'
 
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 // No __dirname under Node ESM
 const __filename = fileURLToPath(import.meta.url)
@@ -15,7 +15,8 @@ export default defineConfig({
     setupFiles: ['./vitest.setup.js'],
     include: ['./src/**/*.(spec|test).[jt]s?(x)'],
     alias: {
-      '@reduxjs/toolkit/query/react': path.join(__dirname, './src/query/react/index.ts'), // @remap-prod-remove-line
+      // prettier-ignore
+      '@reduxjs/toolkit/query/react': path.join(__dirname,'./src/query/react/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit/query': path.join(__dirname, './src/query/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit/react': path.join(__dirname, './src/index.ts'), // @remap-prod-remove-line
       '@reduxjs/toolkit': path.join(__dirname, './src/index.ts'), // @remap-prod-remove-line
