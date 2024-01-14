5e04ec324b502af47e5b1eed6b00438cdf55ef58
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-14 20:58:19 +0000

    add comment slice to demonstrate linked normalized state

--- examples/lazy-injection/counter-todo/src/features/todos/Todo.tsx
+++ examples/lazy-injection/counter-todo/src/features/todos/Todo.tsx
@@ -1,20 +1,31 @@
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { deleteTodo, selectTodoById } from "./todoSlice"
 import styles from "./Todos.module.css"
+import { AddComment } from "./AddComment"
+import { selectCommentsByTodoId } from "./commentSlice"
+import { CommentDisplay } from "./Comment"
 
 export function Todo({ id }: { id: string }) {
   const dispatch = useAppDispatch()
   const todo = useAppSelector(state => selectTodoById(state, id))
+  const comments = useAppSelector(state => selectCommentsByTodoId(state, id))
   if (!todo) return null
   return (
     <div className={styles.todo}>
-      <p>{todo.title}</p>
-      <button
-        className={styles.button}
-        onClick={() => dispatch(deleteTodo(id))}
-      >
-        Delete
-      </button>
+      <div className={styles.todoHeader}>
+        <p>{todo.title}</p>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(deleteTodo(id))}
+        >
+          Delete
+        </button>
+      </div>
+      <hr />
+      {comments.map(comment => (
+        <CommentDisplay key={comment.id} comment={comment} />
+      ))}
+      <AddComment todoId={id} />
     </div>
   )
 }
---
+++ examples/lazy-injection/counter-todo/src/features/todos/commentSlice.ts
@@ -0,0 +1,60 @@
+import type { EntityState } from "@reduxjs/toolkit"
+import { createEntityAdapter, createSelector, nanoid } from "@reduxjs/toolkit"
+import { createAppSlice } from "../../app/createAppSlice"
+import { deleteTodo } from "./todoSlice"
+
+export interface Comment {
+  id: string
+  todoId: string
+  message: string
+}
+
+const commentAdapter = createEntityAdapter<Comment>()
+
+const localisedSelectors = commentAdapter.getSelectors()
+
+export const commentSlice = createAppSlice({
+  name: "comments",
+  initialState: commentAdapter.getInitialState(),
+  reducers: {
+    addComment: {
+      reducer: commentAdapter.setOne,
+      prepare: (comment: Omit<Comment, "id">) => ({
+        payload: { id: nanoid(), ...comment },
+      }),
+    },
+    deleteComment: commentAdapter.removeOne,
+  },
+  extraReducers: builder => {
+    builder.addCase(deleteTodo, (state, action) => {
+      commentAdapter.removeMany(
+        state,
+        state.ids.filter(id => state.entities[id]?.todoId === action.payload),
+      )
+    })
+  },
+  selectors: {
+    selectAllComments: localisedSelectors.selectAll,
+    selectCommentById: localisedSelectors.selectById,
+    selectCommentEntities: localisedSelectors.selectEntities,
+    selectCommentIds: localisedSelectors.selectIds,
+    selectCommentTotal: localisedSelectors.selectTotal,
+    selectCommentsByTodoId: createSelector(
+      localisedSelectors.selectAll,
+      (_state: EntityState<Comment, string>, todoId: string) => todoId,
+      (comments, todoId) =>
+        comments.filter(comment => comment.todoId === todoId),
+    ),
+  },
+})
+
+export const { addComment, deleteComment } = commentSlice.actions
+
+export const {
+  selectAllComments,
+  selectCommentById,
+  selectCommentEntities,
+  selectCommentIds,
+  selectCommentTotal,
+  selectCommentsByTodoId,
+} = commentSlice.selectors
