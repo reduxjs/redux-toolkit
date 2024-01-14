d8aa0dbdd88ea299ad548760a5c1cb0e1cebd6f6
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-14 20:58:19 +0000

    add comment slice to demonstrate linked normalized state

--- examples/lazy-injection/counter-todo/src/app/reducer.ts
+++ examples/lazy-injection/counter-todo/src/app/reducer.ts
@@ -1,9 +1,12 @@
 import { combineSlices } from "@reduxjs/toolkit"
 import { todoSlice } from "../features/todos/todoSlice"
+import { commentSlice } from "../features/todos/commentSlice"
 
 export interface LazyLoadedSlices {}
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-export const rootReducer =
-  combineSlices(todoSlice).withLazyLoadedSlices<LazyLoadedSlices>()
+export const rootReducer = combineSlices(
+  todoSlice,
+  commentSlice,
+).withLazyLoadedSlices<LazyLoadedSlices>()
