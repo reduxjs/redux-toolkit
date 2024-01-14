99253f7f020d16710fd90d33b8a9c4fd774383b3
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-14 19:53:57 +0000

    use withTypes hooks

--- examples/lazy-injection/counter-todo/src/app/hooks.ts
+++ examples/lazy-injection/counter-todo/src/app/hooks.ts
@@ -1,7 +1,9 @@
-import type { TypedUseSelectorHook } from "react-redux"
+// We disable the ESLint rule here because this is the designated place
+// for importing and re-exporting the typed versions of hooks.
+/* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
 import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
