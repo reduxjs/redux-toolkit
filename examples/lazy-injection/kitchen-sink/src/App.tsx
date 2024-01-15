8167c0d96b0c13203945bae324c347c10d2c3f4d
Ben Durrant <ben.durrant@marketdojo.com>
2024-01-15 15:22:18 +0000

    add tabs

--- examples/lazy-injection/kitchen-sink/src/App.tsx
+++ examples/lazy-injection/kitchen-sink/src/App.tsx
@@ -1,8 +1,9 @@
-import { Suspense, useReducer, useTransition } from "react"
-import "./App.css"
+import { Suspense, useState, useTransition } from "react"
 import logo from "./logo.svg"
 import { Todos } from "./features/todos/Todos"
 import { lazily } from "react-lazily"
+import { Tab } from "./components/Tab"
+import "./App.css"
 
 // equivalent to
 // const Counter = lazy(() => import("./features/counter/Counter").then(m => ({ default: m.Counter }))
@@ -11,33 +12,15 @@ const { Counter } = lazily(() => import("./features/counter/Counter"))
 const { Quotes } = lazily(() => import("./features/quotes/Quotes"))
 
 const App = () => {
-  const [counterOpen, toggleCounter] = useReducer(b => !b, false)
-  const [quotesOpen, toggleQuotes] = useReducer(b => !b, false)
+  const [tab, setTab] = useState("todos")
   const [, startTransition] = useTransition()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Todos />
-        <details open={counterOpen}>
-          <summary onClick={() => startTransition(toggleCounter)}>
-            Counter example (lazy)
-          </summary>
-          <Suspense fallback="Loading counter">
-            {counterOpen && <Counter />}
-          </Suspense>
-        </details>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <details open={quotesOpen}>
-          <summary onClick={() => startTransition(toggleQuotes)}>
-            Quotes example (lazy)
-          </summary>
-          <Suspense fallback="Loading quotes">
-            {quotesOpen && <Quotes />}
-          </Suspense>
-        </details>
         <span>
           <span>Learn </span>
           <a
@@ -86,6 +69,37 @@ const App = () => {
           </a>
         </span>
       </header>
+      <div className="tabs">
+        <Tab
+          selected={tab === "todos"}
+          onClick={() => startTransition(() => setTab("todos"))}
+        >
+          Todos
+        </Tab>
+        <Tab
+          selected={tab === "counter"}
+          onClick={() => startTransition(() => setTab("counter"))}
+        >
+          Counter
+        </Tab>
+        <Tab
+          selected={tab === "quotes"}
+          onClick={() => startTransition(() => setTab("quotes"))}
+        >
+          Quotes
+        </Tab>
+      </div>
+      {tab === "todos" && <Todos />}
+      {tab === "counter" && (
+        <Suspense fallback="Loading counter">
+          <Counter />
+        </Suspense>
+      )}
+      {tab === "quotes" && (
+        <Suspense fallback="Loading quotes">
+          <Quotes />
+        </Suspense>
+      )}
     </div>
   )
 }
