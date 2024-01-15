8dd9609ba5f6c9ba61dc89a063ee2d3c130dd984
Arya Emami <aryaemami59@yahoo.com>
2024-01-14 21:54:30 -0600

    Copy `transformTestUtils.ts` from Reselect codemod

--- packages/rtk-codemods/transformTestUtils.ts
+++ packages/rtk-codemods/transformTestUtils.ts
@@ -1,61 +1,63 @@
-import { describe, vi } from 'vitest';
-import type { Transform } from 'jscodeshift';
-import globby from 'globby';
-import fs from 'fs-extra';
-import path from 'path';
-import { runInlineTest } from 'jscodeshift/dist/testUtils';
+import { globbySync } from 'globby'
+import type { Transform } from 'jscodeshift'
+import type { TestOptions } from 'jscodeshift/src/testUtils'
+import { runInlineTest } from 'jscodeshift/src/testUtils'
+import fs from 'node:fs'
+import path from 'node:path'
+import { describe, it } from 'vitest'
 
-export function runTransformTest(
+export const runTransformTest = (
   name: string,
   transform: Transform,
-  parser: string,
+  parser: TestOptions['parser'],
   fixturePath: string
-) {
-  describe(name, function () {
-    globby
-      .sync('**/*.input.*', {
-        cwd: fixturePath,
-        absolute: true,
-      })
+) => {
+  describe(name, () => {
+    globbySync('**/*.input.*', {
+      cwd: fixturePath,
+      absolute: true
+    })
       .map((entry) => entry.slice(fixturePath.length))
       .forEach((filename) => {
-        let extension = path.extname(filename);
-        let testName = filename.replace(`.input${extension}`, '');
-        let testInputPath = path.join(fixturePath, `${testName}${extension}`);
-        let inputPath = path.join(fixturePath, `${testName}.input${extension}`);
-        let outputPath = path.join(fixturePath, `${testName}.output${extension}`);
-        let optionsPath = path.join(fixturePath, `${testName}.options.json`);
-        let options = fs.pathExistsSync(optionsPath) ? fs.readFileSync(optionsPath) : '{}';
+        const extension = path.extname(filename)
+        const testName = filename.replace(`.input${extension}`, '')
+        const testInputPath = path.join(fixturePath, `${testName}${extension}`)
+        const inputPath = path.join(
+          fixturePath,
+          `${testName}.input${extension}`
+        )
+        const outputPath = path.join(
+          fixturePath,
+          `${testName}.output${extension}`
+        )
 
-        describe(testName, function () {
-          beforeEach(function () {
-            process.env.CODEMOD_CLI_ARGS = options;
-          });
-
-          afterEach(function () {
-            process.env.CODEMOD_CLI_ARGS = '';
-          });
-
-          it('transforms correctly', function () {
+        describe(testName, () => {
+          it('transforms correctly', () => {
             runInlineTest(
               transform,
               {},
-              { path: testInputPath, source: fs.readFileSync(inputPath, 'utf8') },
+              {
+                path: testInputPath,
+                source: fs.readFileSync(inputPath, 'utf8')
+              },
               fs.readFileSync(outputPath, 'utf8'),
               { parser }
-            );
-          });
+            )
+          })
 
-          it('is idempotent', function () {
+          it('is idempotent', () => {
             runInlineTest(
               transform,
               {},
-              { path: testInputPath, source: fs.readFileSync(outputPath, 'utf8') },
+              {
+                path: testInputPath,
+                source: fs.readFileSync(outputPath, 'utf8')
+              },
               fs.readFileSync(outputPath, 'utf8'),
               { parser }
-            );
-          });
-        });
-      });
-  });
+            )
+          })
+        })
+      })
+  })
 }
