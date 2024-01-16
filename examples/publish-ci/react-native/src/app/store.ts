510c1312e360dff1186f19deceb633289983beea
Arya Emami <aryaemami59@yahoo.com>
2024-01-15 20:45:11 -0600

    Copy `store.ts` from react-native-template-redux-typescript

--- examples/publish-ci/react-native/src/app/store.ts
+++ examples/publish-ci/react-native/src/app/store.ts
@@ -10,6 +10,8 @@ const rootReducer = combineSlices(counterSlice, quotesApiSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
+// The store setup is wrapped in `makeStore` to allow reuse
+// when setting up tests that need the same store config
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
