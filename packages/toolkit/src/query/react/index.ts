052a5120aed226a7084b073c73fb83305bc3c3e4
Arya Emami <aryaemami59@yahoo.com>
2024-01-20 08:46:25 -0600

    Add back `formatProdErrorMessage` to `src\query\react\index.ts`

--- packages/toolkit/src/query/react/index.ts
+++ packages/toolkit/src/query/react/index.ts
@@ -1,5 +1,6 @@
 // This must remain here so that the `mangleErrors.cjs` build script
 // does not have to import this into each source file it rewrites.
+import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
 import { buildCreateApi, coreModule } from '@reduxjs/toolkit/query'
 import { reactHooksModule, reactHooksModuleName } from './module'
