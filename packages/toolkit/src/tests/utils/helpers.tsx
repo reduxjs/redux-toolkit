5f1451399a13816ba20f31bd098d858b35f94d42
Arya Emami <aryaemami59@yahoo.com>
2024-01-23 05:40:52 -0600

    Try to fix ambient declarations issue

--- packages/toolkit/src/tests/utils/helpers.tsx
+++ packages/toolkit/src/tests/utils/helpers.tsx
@@ -18,6 +18,18 @@ import {
   mockConsole,
 } from 'console-testing-library/pure'
 
+import type { Assertion, AsymmetricMatchersContaining } from 'vitest'
+
+interface CustomMatchers<R = unknown> {
+  toMatchSequence(...matchers: Array<(arg: any) => boolean>): R
+  toHaveConsoleOutput(expectedOutput: string): Promise<R>
+}
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
+
 export const ANY = 0 as any
 
 export const DEFAULT_DELAY_MS = 150
