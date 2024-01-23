aa0eef762f7f7e52b04ab43caa973f16b66f1897
Arya Emami <aryaemami59@yahoo.com>
2024-01-22 19:54:22 -0600

    Fix stubbing globals in `vitest.setup.ts`

--- packages/toolkit/vitest.setup.ts
+++ packages/toolkit/vitest.setup.ts
@@ -1,19 +1,18 @@
-//@ts-ignore
-import nodeFetch from 'node-fetch'
-//@ts-ignore
-globalThis.fetch = nodeFetch
-//@ts-ignore
-globalThis.Request = nodeFetch.Request
-globalThis.Headers = nodeFetch.Headers
+import nodeFetch, { Headers, Request } from 'node-fetch'
 import { server } from './src/query/tests/mocks/server'
 
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+vi.stubGlobal('fetch', nodeFetch)
+vi.stubGlobal('Request', Request)
+vi.stubGlobal('Headers', Headers)
 
-process.on('unhandledRejection', (error) => {
-  // eslint-disable-next-line no-undef
-  fail(error)
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' })
 })
 
-process.env.NODE_ENV = 'development'
+afterEach(() => {
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
