be4e03e24965acb2ac1ea1aa0400ac3e1aee9db2
Arya Emami <aryaemami59@yahoo.com>
2024-01-23 09:24:30 -0600

    Remove `interopDefault` from `vitest.config.ts` as it is enabled by default

--- packages/rtk-codemods/vitest.config.ts
+++ packages/rtk-codemods/vitest.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
-    watch: false,
-    deps: {
-      interopDefault: true,
-    },
-  },
-});
+    watch: false
+  }
+})
