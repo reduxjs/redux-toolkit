81c1ad741a18c66dca716ef7b65396786aafb926
Arya Emami <aryaemami59@yahoo.com>
2024-01-25 04:18:50 -0600

    Add global module augmentation for `namespace jest`
      - This changes was made because TypeScript 4.7 and 4.8 need an explicit global module augmentation for the `jest` namespace.

--- packages/toolkit/src/tests/utils/CustomMatchers.d.ts
+++ packages/toolkit/src/tests/utils/CustomMatchers.d.ts
@@ -9,3 +9,9 @@ declare module "vitest" {
   interface Assertion<T = any> extends CustomMatchers<T> {}
   interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+  }
+}
