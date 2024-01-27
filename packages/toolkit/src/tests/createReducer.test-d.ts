6d309df75381c6594f342d9996515b835ecca5e9
Arya Emami <aryaemami59@yahoo.com>
2024-01-27 00:21:50 -0600

    Migrate type tests for `src/tests/createReducer.test-d.ts` to Vitest

--- packages/toolkit/src/tests/createReducer.test-d.ts
+++ packages/toolkit/src/tests/createReducer.test-d.ts
@@ -1,84 +1,75 @@
 import type { ActionReducerMapBuilder } from '@reduxjs/toolkit'
 import { createAction, createReducer } from '@reduxjs/toolkit'
 import type { Reducer } from 'redux'
-import { expectType } from './utils/typeTestHelpers'
 
-/*
- * Test: createReducer() infers type of returned reducer.
- */
-{
-  const incrementHandler = (
-    state: number,
-    action: { type: 'increment'; payload: number }
-  ) => state + 1
-  const decrementHandler = (
-    state: number,
-    action: { type: 'decrement'; payload: number }
-  ) => state - 1
+describe('type tests', () => {
+  test('createReducer() infers type of returned reducer.', () => {
+    const incrementHandler = (
+      state: number,
+      action: { type: 'increment'; payload: number }
+    ) => state + 1
+    const decrementHandler = (
+      state: number,
+      action: { type: 'decrement'; payload: number }
+    ) => state - 1
 
-  const reducer = createReducer(0 as number, (builder) => {
-    builder
-      .addCase('increment', incrementHandler)
-      .addCase('decrement', decrementHandler)
-  })
+    const reducer = createReducer(0 as number, (builder) => {
+      builder
+        .addCase('increment', incrementHandler)
+        .addCase('decrement', decrementHandler)
+    })
 
-  const numberReducer: Reducer<number> = reducer
+    const numberReducer: Reducer<number> = reducer
 
-  // @ts-expect-error
-  const stringReducer: Reducer<string> = reducer
-}
-
-/**
- * Test: createReducer() state type can be specified expliclity.
- */
-{
-  const incrementHandler = (
-    state: number,
-    action: { type: 'increment'; payload: number }
-  ) => state + action.payload
-
-  const decrementHandler = (
-    state: number,
-    action: { type: 'decrement'; payload: number }
-  ) => state - action.payload
-
-  createReducer(0 as number, (builder) => {
-    builder
-      .addCase('increment', incrementHandler)
-      .addCase('decrement', decrementHandler)
-  })
-
-  // @ts-expect-error
-  createReducer<string>(0 as number, (builder) => {
     // @ts-expect-error
-    builder
-      .addCase('increment', incrementHandler)
-      .addCase('decrement', decrementHandler)
+    const stringReducer: Reducer<string> = reducer
   })
-}
 
-/*
- * Test: createReducer() ensures state type is mutable within a case reducer.
- */
-{
-  const initialState: { readonly counter: number } = { counter: 0 }
+  test('createReducer() state type can be specified expliclity.', () => {
+    const incrementHandler = (
+      state: number,
+      action: { type: 'increment'; payload: number }
+    ) => state + action.payload
 
-  createReducer(initialState, (builder) => {
-    builder.addCase('increment', (state) => {
-      state.counter += 1
+    const decrementHandler = (
+      state: number,
+      action: { type: 'decrement'; payload: number }
+    ) => state - action.payload
+
+    createReducer(0 as number, (builder) => {
+      builder
+        .addCase('increment', incrementHandler)
+        .addCase('decrement', decrementHandler)
+    })
+
+    // @ts-expect-error
+    createReducer<string>(0 as number, (builder) => {
+      // @ts-expect-error
+      builder
+        .addCase('increment', incrementHandler)
+        .addCase('decrement', decrementHandler)
     })
   })
-}
 
-/** Test: builder callback for actionMap */
-{
-  const increment = createAction<number, 'increment'>('increment')
+  test('createReducer() ensures state type is mutable within a case reducer.', () => {
+    const initialState: { readonly counter: number } = { counter: 0 }
 
-  const reducer = createReducer(0, (builder) =>
-    expectType<ActionReducerMapBuilder<number>>(builder)
-  )
+    createReducer(initialState, (builder) => {
+      builder.addCase('increment', (state) => {
+        state.counter += 1
+      })
+    })
+  })
 
-  expectType<number>(reducer(0, increment(5)))
-  // @ts-expect-error
-  expectType<string>(reducer(0, increment(5)))
-}
+  test('builder callback for actionMap', () => {
+    const increment = createAction<number, 'increment'>('increment')
+
+    const reducer = createReducer(0, (builder) =>
+      expectTypeOf(builder).toEqualTypeOf<ActionReducerMapBuilder<number>>()
+    )
+
+    expectTypeOf(reducer(0, increment(5))).toBeNumber()
+
+    expectTypeOf(reducer(0, increment(5))).not.toBeString()
+  })
+})
