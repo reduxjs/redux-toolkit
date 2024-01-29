1435e486e025addc29cbd956c35b0ef69d3fb962
Arya Emami <aryaemami59@yahoo.com>
2024-01-29 11:28:56 -0600

    Update msw code snippets with v2 syntax

--- packages/rtk-query-codegen-openapi/test/mocks/server.ts
+++ packages/rtk-query-codegen-openapi/test/mocks/server.ts
@@ -1,17 +1,43 @@
+import { headersToObject } from 'headers-polyfill';
+import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
-import { rest } from 'msw';
-
 import petstoreJSON from '../fixtures/petstore.json';
 import petstoreYAML from '../fixtures/petstore.yaml.mock';
 
 // This configures a request mocking server with the given request handlers.
 
 export const server = setupServer(
-  rest.get('https://example.com/echo', (req, res, ctx) => res(ctx.json({ ...req, headers: req.headers.all() }))),
-  rest.post('https://example.com/echo', (req, res, ctx) => res(ctx.json({ ...req, headers: req.headers.all() }))),
+  http.get('https://example.com/echo', async ({ request, params, cookies, requestId }) =>
+    HttpResponse.json({
+      ...request,
+      params,
+      cookies,
+      requestId,
+      url: new URL(request.url),
+      headers: headersToObject(request.headers),
+    })
+  ),
+  http.post('https://example.com/echo', async ({ request, params, cookies, requestId }) => {
+    let body;
 
-  rest.get('https://petstore3.swagger.io/api/v3/openapi.json', (req, res, ctx) => res(ctx.json(petstoreJSON))),
-  rest.get('https://petstore3.swagger.io/api/v3/openapi.yaml', (req, res, ctx) =>
-    res(ctx.text(petstoreYAML), ctx.set('Content-Type', 'application/yaml'))
-  )
+    try {
+      body =
+        headersToObject(request.headers)['content-type'] === 'text/html' ? await request.text() : await request.json();
+    } catch (err) {
+      body = request.body;
+    }
+
+    return HttpResponse.json({
+      ...request,
+      cookies,
+      params,
+      requestId,
+      body,
+      url: new URL(request.url),
+      headers: headersToObject(request.headers),
+    });
+  }),
+
+  http.get('https://petstore3.swagger.io/api/v3/openapi.json', () => HttpResponse.json(petstoreJSON)),
+  http.get('https://petstore3.swagger.io/api/v3/openapi.yaml', () => HttpResponse.text(petstoreYAML))
 );
