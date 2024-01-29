96cb760da21ae2bc01376a30219a6218b85475d1
Ben Durrant <ben.durrant@marketdojo.com>
2024-01-29 16:24:02 +0000

    run prettier

--- examples/lazy-injection/kitchen-sink/src/app/createAppSlice.ts
+++ examples/lazy-injection/kitchen-sink/src/app/createAppSlice.ts
@@ -1,4 +1,4 @@
-import { asyncThunkCreator, buildCreateSlice } from "@reduxjs/toolkit";
+import { asyncThunkCreator, buildCreateSlice } from "@reduxjs/toolkit"
 
 // `buildCreateSlice` allows us to create a slice with async thunks.
 export const createAppSlice = buildCreateSlice({
