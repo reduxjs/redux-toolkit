c51f6569ffa9088a63e4300ad4fbcdbd542dd8bd
Arya Emami <aryaemami59@yahoo.com>
2024-02-03 07:27:57 -0600

    Fix `no-unsafe-optional-chaining` related problems

--- packages/toolkit/src/query/tests/buildHooks.test.tsx
+++ packages/toolkit/src/query/tests/buildHooks.test.tsx
@@ -2027,7 +2027,7 @@ describe('hooks with createApi defaults set', () => {
   const defaultApi = createApi({
     baseQuery: async (arg: any) => {
       await waitMs()
-      if ('amount' in arg?.body) {
+      if ('body' in arg && 'amount' in arg.body) {
         amount += 1
       }
       return {
@@ -2601,7 +2601,7 @@ describe('hooks with createApi defaults set', () => {
     const api = createApi({
       baseQuery: async (arg: any) => {
         await waitMs()
-        if ('amount' in arg?.body) {
+        if ('body' in arg && 'amount' in arg.body) {
           amount += 1
         }
         return {
