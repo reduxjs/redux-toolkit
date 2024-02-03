169c8741a1af648fe926b26d5a0a7e66465bce1d
Arya Emami <aryaemami59@yahoo.com>
2024-02-03 13:00:33 -0600

    Fix `no-constant-condition` related problems

--- packages/toolkit/src/query/retry.ts
+++ packages/toolkit/src/query/retry.ts
@@ -107,6 +107,7 @@ const retryWithBackoff: BaseQueryEnhancer<
   }
   let retry = 0
 
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     try {
       const result = await baseQuery(args, api, extraOptions)
