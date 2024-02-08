05cc7a517a5956ebd9359a762a83ff694e612f80
Arya Emami <aryaemami59@yahoo.com>
2024-02-08 00:57:46 -0600

    De-duplicate `AnyFunction` definition

--- packages/toolkit/src/entities/state_selectors.ts
+++ packages/toolkit/src/entities/state_selectors.ts
@@ -1,11 +1,15 @@
-import type { CreateSelectorFunction, Selector } from 'reselect'
+import type {
+  CreateSelectorFunction,
+  Selector,
+  UnknownMemoizer,
+} from 'reselect'
 import { createDraftSafeSelector } from '../createDraftSafeSelector'
+import type { AnyFunction } from '../tsHelpers'
 import type { EntityId, EntitySelectors, EntityState } from './models'
 
-type AnyFunction = (...args: any) => any
 type AnyCreateSelectorFunction = CreateSelectorFunction<
-  <F extends AnyFunction>(f: F) => F,
-  <F extends AnyFunction>(f: F) => F
+  UnknownMemoizer<AnyFunction>,
+  UnknownMemoizer<AnyFunction>
 >
 
 export interface GetSelectorsOptions {
