7118f1ac252b228d43ddd872f22ace6a51acbdf9
Ben Durrant <ben.durrant@marketdojo.com>
2024-02-12 13:45:05 +0000

    remove unused barrel file

--- packages/toolkit/src/entities/index.ts
+++
@@ -1,9 +0,0 @@
-export { createEntityAdapter } from './create_adapter'
-export type {
-  EntityState,
-  EntityAdapter,
-  Update,
-  IdSelector,
-  Comparer,
-} from './models'
-export { entityMethodsCreator, entityMethodsCreatorType } from './slice_creator'
