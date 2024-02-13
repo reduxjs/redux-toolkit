e18ca7e706efdced92892f337f3b7ab53e9552e6
Arya Emami <aryaemami59@yahoo.com>
2024-02-13 12:20:27 -0600

    Fix `eslint.legacy.config.ts`

--- packages/configs/eslint/eslint.legacy.config.ts
+++ packages/configs/eslint/eslint.legacy.config.ts
@@ -1,22 +1,32 @@
 import type { Linter } from 'eslint'
 
-const eslintConfig: Linter.Config = {
+export const vitestGlobals = {
+  suite: true,
+  test: true,
+  describe: true,
+  it: true,
+  expectTypeOf: true,
+  assertType: true,
+  expect: true,
+  assert: true,
+  vitest: true,
+  vi: true,
+  beforeAll: true,
+  afterAll: true,
+  beforeEach: true,
+  afterEach: true,
+} satisfies Record<string, boolean>
+
+export const reduxESLintLegacyConfig: Linter.Config = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/stylistic',
-    // 'plugin:@typescript-eslint/recommended-type-checked',
-    // 'plugin:@typescript-eslint/stylistic-type-checked',
     'prettier',
   ],
-  env: { jest: true, browser: true, node: true, es2024: true },
+  env: { browser: true, node: true, es2024: true },
   globals: {
-    suite: true,
-    expectTypeOf: true,
-    assertType: true,
-    assert: true,
-    vitest: true,
-    vi: true,
+    ...vitestGlobals,
   },
   rules: {
     '@typescript-eslint/consistent-type-imports': [
@@ -29,23 +39,7 @@ const eslintConfig: Linter.Config = {
     '@typescript-eslint/no-explicit-any': [0],
     '@typescript-eslint/no-empty-interface': [2, { allowSingleExtends: true }],
     '@typescript-eslint/no-unsafe-argument': [0],
-    '@typescript-eslint/ban-types': [
-      2,
-      // {
-      //   extendDefaults: true,
-      //   types: {
-      //     '{}': { fixWith: 'AnyNonNullishValue' },
-      //     Function: { fixWith: 'AnyFunction' },
-      //   },
-      // },
-    ],
-    // '@typescript-eslint/prefer-as-const': [2],
-    // '@typescript-eslint/no-unnecessary-type-constraint': [2],
-    // '@typescript-eslint/no-var-requires': [2],
-    // 'no-unsafe-optional-chaining': [2],
-    // '@typescript-eslint/no-unsafe-return': [0],
-    // '@typescript-eslint/no-unsafe-assignment': [0],
-    // '@typescript-eslint/no-empty-function': [0],
+    '@typescript-eslint/ban-types': [2],
     '@typescript-eslint/no-namespace': [
       2,
       { allowDeclarations: true, allowDefinitionFiles: true },
@@ -63,10 +57,11 @@ const eslintConfig: Linter.Config = {
     ],
   },
   parser: '@typescript-eslint/parser',
-  parserOptions: { project: true, ecmaVersion: 'latest' },
+  parserOptions: { project: ['./tsconfig.json'], ecmaVersion: 'latest' },
   plugins: ['@typescript-eslint'],
-  ignorePatterns: ['dist'],
+  ignorePatterns: ['dist', '.*'],
   root: true,
+  reportUnusedDisableDirectives: true,
 }
 
-module.exports = eslintConfig
+module.exports = reduxESLintLegacyConfig
