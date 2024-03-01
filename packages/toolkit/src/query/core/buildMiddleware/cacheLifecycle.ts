232dc68919d015de1901d15c7ce74ff84b0a9427
Jackson <jacksonbrummell1@gmail.com>
2024-03-01 15:36:34 +1100

    Added rough draft of initial endpoint definition for infinite query

--- packages/toolkit/src/query/core/buildMiddleware/cacheLifecycle.ts
+++ packages/toolkit/src/query/core/buildMiddleware/cacheLifecycle.ts
@@ -142,6 +142,20 @@ export type CacheLifecycleQueryExtraOptions<
   ): Promise<void> | void
 }
 
+// copying QueryDefinition to get past initial build
+interface InfiniteQueryExtraOptions<
+  TagTypes extends string,
+  ResultType,
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ReducerPath extends string = string,
+> {
+  onCacheEntryAdded?(
+    arg: QueryArg,
+    api: QueryCacheLifecycleApi<QueryArg, BaseQuery, ResultType, ReducerPath>,
+  ): Promise<void> | void
+}
+
 export type CacheLifecycleMutationExtraOptions<
   ResultType,
   QueryArg,
