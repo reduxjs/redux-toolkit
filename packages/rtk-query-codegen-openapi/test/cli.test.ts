9f6d565b7c53a48630718ce44e59b241806f11e8
Arya Emami <aryaemami59@yahoo.com>
2024-03-27 00:07:44 -0500

    Fix codegen tests

--- packages/rtk-query-codegen-openapi/test/cli.test.ts
+++ packages/rtk-query-codegen-openapi/test/cli.test.ts
@@ -1,76 +1,91 @@
-import del from 'del';
-import type { ExecException } from 'node:child_process';
-import { exec } from 'node:child_process';
-import fs from 'node:fs';
+import { exec as _exec } from 'node:child_process';
+import fs from 'node:fs/promises';
 import path from 'node:path';
+import { promisify } from 'node:util';
+import { rimraf } from 'rimraf';
 
-function cli(args: string[], cwd: string): Promise<{ error: ExecException | null; stdout: string; stderr: string }> {
-  const pwd = process.env?.PWD || '.';
-  const cmd = `yarn cli ${args.join(' ')}`;
-  return new Promise((resolve) => {
-    exec(cmd, { cwd }, (error, stdout, stderr) => {
-      resolve({
-        error,
-        stdout,
-        stderr,
-      });
-    });
-  });
-}
+const exec = promisify(_exec);
+
+const cli = async (args: string[]) => {
+  return await exec(`node ${path.resolve('lib', 'bin', 'cli.js')} ${args.join(' ')}`);
+};
 
 const tmpDir = path.resolve(__dirname, 'tmp');
 
+export const isDir = async (filePath: string) => {
+  try {
+    const stat = await fs.lstat(filePath);
+    return stat.isDirectory();
+  } catch {
+    return false;
+  }
+};
+
+const convertSpecialCharsToHyphens = (str: string) => str.replace(/[^a-zA-Z0-9]+/g, '-');
+
 describe('CLI options testing', () => {
-  beforeAll(() => {
-    if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir, { recursive: true });
+  beforeAll(async () => {
+    if (!(await isDir(tmpDir))) {
+      await fs.mkdir(tmpDir, { recursive: true });
+    }
   });
 
-  afterEach(() => {
-    del.sync(`${tmpDir}/*.ts`);
+  beforeEach(async ({ task }) => {
+    const sanitizedPath = path.join(tmpDir, convertSpecialCharsToHyphens(task.name));
+    if (!(await isDir(sanitizedPath))) {
+      await fs.mkdir(sanitizedPath, { recursive: true });
+    }
+  });
+
+  afterEach(async () => {
+    await rimraf(`${tmpDir}/*.ts`, { glob: true });
+  });
+
+  afterAll(async () => {
+    await rimraf(tmpDir);
   });
 
   test('generation with `config.example.js`', async () => {
-    const out = await cli(['./test/config.example.js'], __dirname);
+    const out = await cli(['./test/config.example.js']);
 
     expect(out).toEqual({
       stdout: `Generating ./tmp/example.ts
 Done
 `,
       stderr: '',
-      error: null,
     });
 
-    expect(fs.readFileSync(path.resolve(tmpDir, 'example.ts'), 'utf-8')).toMatchSnapshot();
+    expect(await fs.readFile(path.resolve(tmpDir, 'example.ts'), 'utf-8')).toMatchSnapshot();
   }, 25_000);
 
   test('paths are relative to config file, not to cwd', async () => {
-    const out = await cli([`./test/config.example.js`], __dirname);
+    const out = await cli([`./test/config.example.js`]);
 
     expect(out).toEqual({
       stdout: `Generating ./tmp/example.ts
 Done
 `,
       stderr: '',
-      error: null,
     });
 
-    expect(fs.readFileSync(path.resolve(tmpDir, 'example.ts'), 'utf-8')).toMatchSnapshot();
+    expect(await fs.readFile(path.resolve(tmpDir, 'example.ts'), 'utf-8')).toMatchSnapshot();
   }, 25_000);
 
   test('ts, js and json all work the same', async () => {
-    await cli([`./test/config.example.js`], __dirname);
-    const fromJs = fs.readFileSync(path.resolve(tmpDir, 'example.ts'), 'utf-8');
-    await cli([`./test/config.example.ts`], __dirname);
-    const fromTs = fs.readFileSync(path.resolve(tmpDir, 'example.ts'), 'utf-8');
-    await cli([`./test/config.example.json`], __dirname);
-    const fromJson = fs.readFileSync(path.resolve(tmpDir, 'example.ts'), 'utf-8');
+    await cli([`./test/config.example.js`]);
+    const fromJs = await fs.readFile(path.resolve(tmpDir, 'example.ts'), 'utf-8');
+    await cli([`./test/config.example.ts`]);
+    const fromTs = await fs.readFile(path.resolve(tmpDir, 'example.ts'), 'utf-8');
+    await cli([`./test/config.example.json`]);
+    const fromJson = await fs.readFile(path.resolve(tmpDir, 'example.ts'), 'utf-8');
 
     expect(fromTs).toEqual(fromJs);
     expect(fromJson).toEqual(fromJs);
   }, 120_000);
 
   test("missing parameters doesn't fail", async () => {
-    const out = await cli([`./test/config.invalid-example.json`], __dirname);
-    expect(out.stderr).toContain("Error: path parameter petId does not seem to be defined in '/pet/{petId}'!");
+    await expect(() => cli([`./test/config.invalid-example.json`])).rejects.toThrowError(
+      "Error: path parameter petId does not seem to be defined in '/pet/{petId}'!"
+    );
   }, 25_000);
 });
