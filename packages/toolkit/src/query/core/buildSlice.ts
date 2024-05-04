fd2c6a18225dbf203024728874a6e83351b0bd27
riqts <jacksonbrummell1@gmail.com>
2024-05-04 20:49:40 +1000

    Added necessary slice actions for inf query substate

--- packages/toolkit/src/query/core/buildSlice.ts
+++ packages/toolkit/src/query/core/buildSlice.ts
@@ -21,6 +21,7 @@ import type {
   QueryCacheKey,
   SubscriptionState,
   ConfigState,
+  InfiniteQuerySubState
 } from './apiState'
 import { QueryStatus } from './apiState'
 import type { InfiniteQueryThunk, MutationThunk, QueryThunk, RejectedAction } from './buildThunks'
@@ -46,7 +47,7 @@ import { isUpsertQuery } from './buildInitiate'
 function updateQuerySubstateIfExists(
   state: QueryState<any>,
   queryCacheKey: QueryCacheKey,
-  update: (substate: QuerySubState<any>) => void,
+  update: (substate: QuerySubState<any> | InfiniteQuerySubState<any>) => void,
 ) {
   const substate = state[queryCacheKey]
   if (substate) {
@@ -168,6 +169,14 @@ export function buildSlice({
               substate.originalArgs = arg.originalArgs
             }
             substate.startedTimeStamp = meta.startedTimeStamp
+
+            // TODO: Awful - fix this most likely by just moving it to its own slice that only works on InfQuery's
+            if ('param' in substate && 'direction' in substate) {
+              if ('param' in arg && 'direction' in arg) {
+                substate.param = arg.param
+                substate.direction = arg.direction as 'forward' | 'backwards' | undefined
+              }
+            }
           })
         })
         .addCase(queryThunk.fulfilled, (draft, { meta, payload }) => {
@@ -422,78 +431,6 @@ export function buildSlice({
         )
     },
   })
-
-  const infiniteQuerySlice = createSlice({
-    name: `${reducerPath}/infinitequeries`,
-    initialState: initialState as QueryState<any>,
-    reducers: {
-      fetchNextPage(
-        d,
-        a: PayloadAction<
-          {
-            endpointName: string
-            requestId: string
-            options: Subscribers[number]
-          } & QuerySubstateIdentifier
-        >,
-      ) {
-        // Dummy
-      },
-      unsubscribeQueryResult(
-        d,
-        a: PayloadAction<{ requestId: string } & QuerySubstateIdentifier>,
-      ) {
-        // Dummy
-      },
-      internal_getRTKQSubscriptions() {},
-    },
-    // extraReducers(builder) {
-    //   builder
-    //     .addCase(queryThunk.fulfilled, (draft, { meta, payload }) => {
-    //       updateQuerySubstateIfExists(
-    //         draft,
-    //         meta.arg.queryCacheKey,
-    //         (substate) => {
-    //           const { infiniteQueryOptions } = definitions[
-    //             meta.arg.endpointName
-    //             ] as InfiniteQueryDefinition<any, any, any, any>
-    //           substate.status = QueryStatus.fulfilled
-    //           if(!infiniteQueryOptions) return
-    //
-    //             if (substate.data !== undefined) {
-    //               const { fulfilledTimeStamp, arg, baseQueryMeta, requestId } =
-    //                 meta
-    //               // There's existing cache data. Let the user merge it in themselves.
-    //               // We're already inside an Immer-powered reducer, and the user could just mutate `substate.data`
-    //               // themselves inside of `merge()`. But, they might also want to return a new value.
-    //               // Try to let Immer figure that part out, save the result, and assign it to `substate.data`.
-    //               substate.data = payload
-    //             } else {
-    //               // Presumably a fresh request. Just cache the response data.
-    //               substate.data = payload
-    //             }
-    //           } else {
-    //             // Assign or safely update the cache data.
-    //             substate.data =
-    //               definitions[meta.arg.endpointName].structuralSharing ?? true
-    //                 ? copyWithStructuralSharing(
-    //                   isDraft(substate.data)
-    //                     ? original(substate.data)
-    //                     : substate.data,
-    //                   payload,
-    //                 )
-    //                 : payload
-    //           }
-    //
-    //           delete substate.error
-    //           substate.fulfilledTimeStamp = meta.fulfilledTimeStamp
-    //         },
-    //       )
-    //     })
-
-    // },
-  })
-
 
   // Dummy slice to generate actions
   const subscriptionSlice = createSlice({
