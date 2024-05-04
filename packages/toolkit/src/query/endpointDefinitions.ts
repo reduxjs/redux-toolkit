ae9b98a6b6b0e9d6c6d7a1899b7e2a4799948bb7
riqts <jacksonbrummell1@gmail.com>
2024-05-04 20:52:07 +1000

    Altered EndPointDefinition for type inference

--- packages/toolkit/src/query/endpointDefinitions.ts
+++ packages/toolkit/src/query/endpointDefinitions.ts
@@ -1,7 +1,5 @@
 import type { Api } from '@reduxjs/toolkit/query'
 import type {
-  GetNextPageParamFunction,
-  GetPreviousPageParamFunction,
   InfiniteQueryConfigOptions,
   QuerySubState,
   RootState,
@@ -584,7 +582,7 @@ export interface InfiniteQueryExtraOptions<
    */
   invalidatesTags?: never
 
-  infiniteQueryOptions: InfiniteQueryConfigOptions
+  infiniteQueryOptions: InfiniteQueryConfigOptions<ResultType, QueryArg>
 
   /**
    * All of these are `undefined` at runtime, purely to be used in TypeScript declarations!
