28af3487bccf908defd6a9c6fa9a8fa9f4683661
Arya Emami <aryaemami59@yahoo.com>
2024-07-19 16:13:47 -0500

    Test `createDynamicMiddleware` and `createListenerMiddleware` exports

--- examples/type-portability/nodenext-cjs/src/app/store.ts
+++ examples/type-portability/nodenext-cjs/src/app/store.ts
@@ -1,13 +1,21 @@
 import ReduxToolkit = require('@reduxjs/toolkit')
+import RTKQuery = require('@reduxjs/toolkit/query')
 import authSliceModule = require('../features/auth/authSlice.js')
 import pollingSliceModule = require('../features/polling/pollingSlice.js')
+import dynamicMiddlewareModule = require('./dynamicMiddleware.js')
+import dynamicReactMiddlewareModule = require('./dynamicReactMiddleware.js')
+import listenerMiddlewareModule = require('./listenerMiddleware.js')
 import apiModule = require('./services/api.js')
 
 namespace storeModule {
   import combineSlices = ReduxToolkit.combineSlices
   import configureStore = ReduxToolkit.configureStore
+  import setupListeners = RTKQuery.setupListeners
   import authSlice = authSliceModule.authSlice
   import pollingSlice = pollingSliceModule.pollingSlice
+  import dynamicMiddleware = dynamicMiddlewareModule.dynamicMiddleware
+  import dynamicReactMiddleware = dynamicReactMiddlewareModule.dynamicReactMiddleware
+  import listenerMiddleware = listenerMiddlewareModule.listenerMiddleware
   import apiSlice = apiModule.apiSlice
 
   export const rootReducer = combineSlices(pollingSlice, authSlice, apiSlice)
@@ -22,12 +30,18 @@ namespace storeModule {
     configureStore({
       reducer: rootReducer,
       middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
+        getDefaultMiddleware()
+          .prepend(listenerMiddleware.middleware)
+          .prepend(dynamicMiddleware.middleware)
+          .prepend(dynamicReactMiddleware.middleware)
+          .concat(apiSlice.middleware),
       preloadedState,
       enhancers: (getDefaultEnhancers) => getDefaultEnhancers(),
     })
 
   export const store = setupStore()
+
+  setupListeners(store.dispatch)
 
   export const { dispatch, getState, replaceReducer, subscribe } = store
 
--- examples/type-portability/nodenext-esm/src/app/store.ts
+++ examples/type-portability/nodenext-esm/src/app/store.ts
@@ -1,6 +1,10 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { authSlice } from '../features/auth/authSlice.js'
 import { pollingSlice } from '../features/polling/pollingSlice.js'
+import { dynamicMiddleware } from './dynamicMiddleware.js'
+import { dynamicReactMiddleware } from './dynamicReactMiddleware.js'
+import { listenerMiddleware } from './listenerMiddleware.js'
 import { apiSlice } from './services/api.js'
 
 export const rootReducer = combineSlices(pollingSlice, authSlice, apiSlice)
@@ -15,12 +19,18 @@ export const setupStore = (preloadedState?: Partial<RootState>) =>
   configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(apiSlice.middleware),
+      getDefaultMiddleware()
+        .prepend(listenerMiddleware.middleware)
+        .prepend(dynamicMiddleware.middleware)
+        .prepend(dynamicReactMiddleware.middleware)
+        .concat(apiSlice.middleware),
     preloadedState,
     enhancers: (getDefaultEnhancers) => getDefaultEnhancers(),
   })
 
 export const store = setupStore()
+
+setupListeners(store.dispatch)
 
 export const { dispatch, getState, replaceReducer, subscribe } = store
 
