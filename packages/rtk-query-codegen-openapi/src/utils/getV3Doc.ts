42aaccd020cbafe7cfb193e30f8fb6226934f166
Kasim Ahmic <KAhmic@ritchiebros.com>
2024-07-20 13:28:44 -0400

    Expose HTTPResolverOptions in GenerationOptions

--- packages/rtk-query-codegen-openapi/src/utils/getV3Doc.ts
+++ packages/rtk-query-codegen-openapi/src/utils/getV3Doc.ts
@@ -3,9 +3,18 @@ import type { OpenAPIV3 } from 'openapi-types';
 // @ts-ignore
 import converter from 'swagger2openapi';
 
-export async function getV3Doc(spec: string): Promise<OpenAPIV3.Document> {
-  const doc = await SwaggerParser.bundle(spec);
+export async function getV3Doc(
+  spec: string,
+  httpResolverOptions?: SwaggerParser.HTTPResolverOptions
+): Promise<OpenAPIV3.Document> {
+  const doc = await SwaggerParser.bundle(spec, {
+    resolve: {
+      http: httpResolverOptions,
+    },
+  });
+
   const isOpenApiV3 = 'openapi' in doc && doc.openapi.startsWith('3');
+
   if (isOpenApiV3) {
     return doc as OpenAPIV3.Document;
   } else {
