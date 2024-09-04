8165e7033edc62815ba771720e2a71094f093468
Arya Emami <aryaemami59@yahoo.com>
2024-09-03 19:07:48 -0500

    Fix exports in `src/query/core/buildMiddleware/index.ts`

--- packages/toolkit/src/query/core/buildMiddleware/index.ts
+++ packages/toolkit/src/query/core/buildMiddleware/index.ts
@@ -26,6 +26,19 @@ import type {
 } from './types'
 import { buildWindowEventHandler } from './windowEventHandling'
 
+export type { ReferenceCacheCollection } from './cacheCollection'
+export type {
+  MutationCacheLifecycleApi,
+  QueryCacheLifecycleApi,
+  ReferenceCacheLifecycle,
+} from './cacheLifecycle'
+export type {
+  MutationLifecycleApi,
+  QueryLifecycleApi,
+  ReferenceQueryLifecycle,
+} from './queryLifecycle'
+export type { SubscriptionSelectors } from './types'
+
 export function buildMiddleware<
   Definitions extends EndpointDefinitions,
   ReducerPath extends string,
