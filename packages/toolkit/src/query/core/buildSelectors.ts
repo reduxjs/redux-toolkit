8033fd0e1043bc2064046dc095a36d569ced288c
Arya Emami <aryaemami59@yahoo.com>
2024-09-03 19:18:07 -0500

    Fix problems related to the `@typescript-eslint/array-type` rule

--- packages/toolkit/src/query/core/buildSelectors.ts
+++ packages/toolkit/src/query/core/buildSelectors.ts
@@ -210,12 +210,12 @@ export function buildSelectors<
 
   function selectInvalidatedBy(
     state: RootState,
-    tags: ReadonlyArray<TagDescription<string> | null | undefined>,
-  ): Array<{
+    tags: readonly (TagDescription<string> | null | undefined)[],
+  ): {
     endpointName: string
     originalArgs: any
     queryCacheKey: QueryCacheKey
-  }> {
+  }[] {
     const apiState = state[reducerPath]
     const toInvalidate = new Set<QueryCacheKey>()
     for (const tag of tags.filter(isNotNullish).map(expandTagDescription)) {
@@ -255,7 +255,7 @@ export function buildSelectors<
   function selectCachedArgsForQuery<QueryName extends QueryKeys<Definitions>>(
     state: RootState,
     queryName: QueryName,
-  ): Array<QueryArgFrom<Definitions[QueryName]>> {
+  ): QueryArgFrom<Definitions[QueryName]>[] {
     return Object.values(state[reducerPath].queries as QueryState<any>)
       .filter(
         (
