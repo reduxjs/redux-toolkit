4efc7189be872e44cc4470873cadd5577cf36c91
Mark Erikson <mark@isquaredsoftware.com>
2024-10-27 16:14:04 +0000

    Pass through `initialPageParam` on thunk dispatch

--- packages/toolkit/src/query/core/buildSlice.ts
+++ packages/toolkit/src/query/core/buildSlice.ts
@@ -160,7 +160,7 @@ export function buildSlice({
 }: {
   reducerPath: string
   queryThunk: QueryThunk
-  infiniteQueryThunk: InfiniteQueryThunk
+  infiniteQueryThunk: InfiniteQueryThunk<any>
   mutationThunk: MutationThunk
   serializeQueryArgs: InternalSerializeQueryArgs
   context: ApiContext<EndpointDefinitions>
