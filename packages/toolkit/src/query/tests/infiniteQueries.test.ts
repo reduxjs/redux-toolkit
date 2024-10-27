6ee32909ed09d6a7d34a87532800e6a7c4c18ac4
Mark Erikson <mark@isquaredsoftware.com>
2024-10-27 13:46:12 +0000

    Fix useInfiniteQuery hook types and trigger behavior

--- packages/toolkit/src/query/tests/infiniteQueries.test.ts
+++ packages/toolkit/src/query/tests/infiniteQueries.test.ts
@@ -1,5 +1,13 @@
 import { configureStore, isAllOf } from '@reduxjs/toolkit'
-import { renderHook, waitFor } from '@testing-library/react'
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { HttpResponse, http } from 'msw'
 import util from 'util'
 import {
@@ -17,57 +25,59 @@ import type { BaseQueryApi } from '../baseQueryTypes'
 import { server } from '@internal/query/tests/mocks/server'
 
 describe('Infinite queries', () => {
+  type Pokemon = {
+    id: string
+    name: string
+  }
+
+  server.use(
+    http.get('https://example.com/listItems', ({ request }) => {
+      const url = new URL(request.url)
+      const pageString = url.searchParams.get('page')
+      const pageNum = parseInt(pageString || '0')
+
+      const results: Pokemon[] = [
+        { id: `${pageNum}`, name: `Pokemon ${pageNum}` },
+      ]
+      return HttpResponse.json(results)
+    }),
+  )
+
+  const pokemonApi = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    endpoints: (builder) => ({
+      // GOAL: Specify both the query arg (for cache key serialization)
+      // and the page param type (for feeding into the query URL)
+      getInfinitePokemon: builder.infiniteQuery<Pokemon[], string, number>({
+        infiniteQueryOptions: {
+          initialPageParam: 0,
+          getNextPageParam: (
+            lastPage,
+            allPages,
+            // Page param type should be `number`
+            lastPageParam,
+            allPageParams,
+          ) => lastPageParam + 1,
+        },
+        // Actual query arg type should be `number`
+        query(pageParam) {
+          return `https://example.com/listItems?page=${pageParam}`
+        },
+      }),
+    }),
+  })
+
+  let storeRef = setupApiStore(pokemonApi, undefined, {
+    withoutTestLifecycles: true,
+  })
+
   beforeEach(() => {
-    server.resetHandlers()
+    storeRef = setupApiStore(pokemonApi, undefined, {
+      withoutTestLifecycles: true,
+    })
   })
 
   test('Basic infinite query behavior', async () => {
-    type Pokemon = {
-      id: string
-      name: string
-    }
-
-    server.use(
-      http.get('https://example.com/listItems', ({ request }) => {
-        const url = new URL(request.url)
-        const pageString = url.searchParams.get('page')
-        const pageNum = parseInt(pageString || '0')
-
-        const results: Pokemon[] = [
-          { id: `${pageNum}`, name: `Pokemon ${pageNum}` },
-        ]
-        return HttpResponse.json(results)
-      }),
-    )
-
-    const pokemonApi = createApi({
-      baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-      endpoints: (builder) => ({
-        // GOAL: Specify both the query arg (for cache key serialization)
-        // and the page param type (for feeding into the query URL)
-        getInfinitePokemon: builder.infiniteQuery<Pokemon[], string, number>({
-          infiniteQueryOptions: {
-            initialPageParam: 0,
-            getNextPageParam: (
-              lastPage,
-              allPages,
-              // Page param type should be `number`
-              lastPageParam,
-              allPageParams,
-            ) => lastPageParam + 1,
-          },
-          // Actual query arg type should be `number`
-          query(pageParam) {
-            return `https://example.com/listItems?page=${pageParam}`
-          },
-        }),
-      }),
-    })
-
-    const storeRef = setupApiStore(pokemonApi, undefined, {
-      withoutTestLifecycles: true,
-    })
-
     const res = storeRef.store.dispatch(
       // Should be `arg: string`
       pokemonApi.endpoints.getInfinitePokemon.initiate('fire', {}),
