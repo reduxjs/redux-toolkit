fed0423ccd319b60502ddd66a6e9e4bb284ad41b
Mark Erikson <mark@isquaredsoftware.com>
2024-11-29 13:13:05 -0500

    Fix bad rebase of exports

--- packages/toolkit/src/query/index.ts
+++ packages/toolkit/src/query/index.ts
@@ -1,7 +1,18 @@
 // This must remain here so that the `mangleErrors.cjs` build script
 // does not have to import this into each source file it rewrites.
+import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
+export type {
+  CombinedState,
+  QueryCacheKey,
+  QueryKeys,
+  QuerySubState,
+  RootState,
+  SubscriptionOptions,
+} from './core/apiState'
+export { QueryStatus } from './core/apiState'
 export type { Api, ApiContext, Module } from './apiTypes'
+
 export type {
   BaseQueryApi,
   BaseQueryArg,
@@ -13,73 +24,35 @@ export type {
   BaseQueryResult,
   QueryReturnValue,
 } from './baseQueryTypes'
-export {
-  QueryStatus,
-  coreModule,
-  coreModuleName,
-  createApi,
-  setupListeners,
-  skipToken,
-} from './core/index'
-export type {
-  ApiEndpointInfiniteQuery,
-  ApiEndpointMutation,
-  ApiEndpointQuery,
-  CombinedState,
-  CoreModule,
-  InfiniteData,
-  InfiniteQueryActionCreatorResult,
-  InfiniteQueryConfigOptions,
-  InfiniteQueryResultSelectorResult,
-  InfiniteQuerySubState,
-  MutationActionCreatorResult,
-  MutationResultSelectorResult,
-  PrefetchOptions,
-  QueryActionCreatorResult,
-  QueryCacheKey,
-  QueryKeys,
-  QueryResultSelectorResult,
-  QuerySubState,
-  RootState,
-  SkipToken,
-  StartQueryActionCreatorOptions,
-  SubscriptionOptions,
-} from './core/index'
-export type { ApiModules } from './core/module'
-export { buildCreateApi } from './createApi'
-export type { CreateApi, CreateApiOptions } from './createApi'
-export { defaultSerializeQueryArgs } from './defaultSerializeQueryArgs'
-export type { SerializeQueryArgs } from './defaultSerializeQueryArgs'
 export type {
   BaseEndpointDefinition,
-  DefinitionType,
-  DefinitionsFromApi,
-  EndpointBuilder,
-  EndpointDefinition,
   EndpointDefinitions,
+  EndpointDefinition,
+  EndpointBuilder,
+  QueryDefinition,
+  MutationDefinition,
+  MutationExtraOptions,
   InfiniteQueryArgFrom,
   InfiniteQueryDefinition,
   InfiniteQueryExtraOptions,
-  MutationDefinition,
-  MutationExtraOptions,
-  OverrideResultType,
   PageParamFrom,
-  QueryArgFrom,
-  QueryDefinition,
-  QueryExtraOptions,
-  ResultDescription,
-  ResultTypeFrom,
   TagDescription,
+  QueryArgFrom,
+  QueryExtraOptions,
+  ResultTypeFrom,
+  DefinitionType,
+  DefinitionsFromApi,
+  OverrideResultType,
+  ResultDescription,
   TagTypesFromApi,
   UpdateDefinitions,
 } from './endpointDefinitions'
-export { _NEVER, fakeBaseQuery } from './fakeBaseQuery'
 export { fetchBaseQuery } from './fetchBaseQuery'
 export type {
-  FetchArgs,
   FetchBaseQueryArgs,
   FetchBaseQueryError,
   FetchBaseQueryMeta,
+  FetchArgs,
 } from './fetchBaseQuery'
 export { retry } from './retry'
 export type { RetryOptions } from './retry'
@@ -91,7 +64,6 @@ export type {
   SkipToken,
 } from './core/buildSelectors'
 export type {
-  InfiniteQueryActionCreatorResult,
   QueryActionCreatorResult,
   MutationActionCreatorResult,
   StartQueryActionCreatorOptions,
@@ -102,6 +74,11 @@ export { _NEVER, fakeBaseQuery } from './fakeBaseQuery'
 export { copyWithStructuralSharing } from './utils/copyWithStructuralSharing'
 export { createApi, coreModule, coreModuleName } from './core/index'
 export type {
+  InfiniteData,
+  InfiniteQueryActionCreatorResult,
+  InfiniteQueryConfigOptions,
+  InfiniteQueryResultSelectorResult,
+  InfiniteQuerySubState,
   TypedMutationOnQueryStarted,
   TypedQueryOnQueryStarted,
 } from './core/index'
@@ -121,4 +98,3 @@ export type {
   NoInfer as TSHelpersNoInfer,
   Override as TSHelpersOverride,
 } from './tsHelpers'
-export { copyWithStructuralSharing } from './utils/copyWithStructuralSharing'
