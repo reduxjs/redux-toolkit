9db26d51777aa20b3b1469e02e265d2689d119d5
Mark Erikson <mark@isquaredsoftware.com>
2024-12-01 13:57:15 -0500

    Add baseApi

--- examples/query/react/infinite-queries/src/app/store.ts
+++ examples/query/react/infinite-queries/src/app/store.ts
@@ -1,10 +1,11 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "../features/baseApi"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices()
+const rootReducer = combineSlices(baseApi)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
@@ -16,7 +17,7 @@ export const makeStore = (preloadedState?: Partial<RootState>) => {
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat()
+      return getDefaultMiddleware().concat(baseApi.middleware)
     },
     preloadedState,
   })
