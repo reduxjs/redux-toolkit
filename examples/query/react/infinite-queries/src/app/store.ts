1120ab742690e74fc86cbeaac58dd24d8a791634
Mark Erikson <mark@isquaredsoftware.com>
2024-12-01 13:24:19 -0500

    Nuke initial examples

--- examples/query/react/infinite-queries/src/app/store.ts
+++ examples/query/react/infinite-queries/src/app/store.ts
@@ -1,12 +1,10 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+const rootReducer = combineSlices()
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
@@ -18,7 +16,7 @@ export const makeStore = (preloadedState?: Partial<RootState>) => {
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+      return getDefaultMiddleware().concat()
     },
     preloadedState,
   })
--- examples/query/react/infinite-queries/src/features/counter/counterAPI.ts
+++
@@ -1,6 +0,0 @@
-// A mock function to mimic making an async request for data
-export const fetchCount = (amount = 1) => {
-  return new Promise<{ data: number }>(resolve =>
-    setTimeout(() => resolve({ data: amount }), 500),
-  )
-}
