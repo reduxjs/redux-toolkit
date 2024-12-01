f0616b09c4640ba81b4641adaa4bd3d035e0c5b5
Mark Erikson <mark@isquaredsoftware.com>
2024-12-01 17:49:32 -0500

    Add RTKQ infinite query example app (#4744)
    * Add blank RTK app
    
    * Nuke initial examples
    
    * Add react-router
    
    * Basic routing
    
    * Add msw
    
    * Configure MSW
    
    * Set up MSW
    
    * Use RTK CSB CI build
    
    * Add baseApi
    
    * Add pagination example
    
    * Add react-intersection-observer
    
    * Add infinite scroll example
    
    * Add max-pages example
    
    * Drop local example lockfile
    
    * Drop back to Vite 5 to fix TS issue
    
    * Align Vite versions to fix test TS error

---
+++ examples/query/react/infinite-queries/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
