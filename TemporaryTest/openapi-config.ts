fd4f1c805480ccdf6c19318eaf3f498046ff73b9
TMoody <137668311+TheMartianMaker@users.noreply.github.com>
2024-12-01 03:39:11 +0000

    added directory for prototyping

---
+++ TemporaryTest/openapi-config.ts
@@ -0,0 +1,13 @@
+openapi-config.ts
+import type { ConfigFile } from '@rtk-query/codegen-openapi'
+
+const config: ConfigFile = {
+  schemaFile: 'https://petstore3.swagger.io/api/v3/openapi.json',
+  apiFile: './src/emptyApi.ts',
+  apiImport: 'emptySplitApi',
+  outputFile: './src/store/petApi.ts',
+  exportName: 'petApi',
+  hooks: true,
+}
+
+export default config
---
+++ TemporaryTest/src/emptyApi.ts
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const emptySplitApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  endpoints: () => ({}),
+})
