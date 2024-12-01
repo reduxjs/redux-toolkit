10347ea1c93fed3d32a9d620cd02e5cf1dbd16de
Mark Erikson <mark@isquaredsoftware.com>
2024-12-01 13:40:18 -0500

    Set up MSW

--- examples/query/react/infinite-queries/src/main.tsx
+++ examples/query/react/infinite-queries/src/main.tsx
@@ -5,20 +5,32 @@ import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
 
-const container = document.getElementById("root")
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return
+  }
 
-if (container) {
-  const root = createRoot(container)
+  const { worker } = await import("./mocks/browser")
 
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
+  return worker.start()
 }
+
+enableMocking().then(() => {
+  const container = document.getElementById("root")
+
+  if (container) {
+    const root = createRoot(container)
+
+    root.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+    )
+  } else {
+    throw new Error(
+      "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    )
+  }
+})
