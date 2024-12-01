cd51528da747004c0c8fd6568ce319d6148ab770
Mark Erikson <mark@isquaredsoftware.com>
2024-12-01 13:31:38 -0500

    Basic routing

--- examples/query/react/infinite-queries/src/App.tsx
+++ examples/query/react/infinite-queries/src/App.tsx
@@ -1,8 +1,39 @@
 import "./App.css"
-import logo from "./logo.svg"
+import { BrowserRouter, Routes, Route, Link } from "react-router"
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Examples</h2>
+      <ul>
+        <li>
+          <Link to="/example-1">Example 1</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Example1 = () => {
+  return (
+    <div>
+      <h2>Example 1</h2>
+    </div>
+  )
+}
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Infinite Query Examples</h1>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/example-1" element={<Example1 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
