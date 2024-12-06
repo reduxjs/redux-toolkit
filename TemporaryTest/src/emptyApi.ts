5699edc1ed8eb0e65656e9e9af180d0f3d4c3c9d
TMoody <137668311+TheMartianMaker@users.noreply.github.com>
2024-12-06 15:51:06 +0000

    removed user created generation testing folder

--- TemporaryTest/src/emptyApi.ts
+++
@@ -1,7 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-// initialize an empty api service that we'll inject endpoints into later as needed
-export const emptySplitApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
-  endpoints: () => ({}),
-})
