45c4437a96e074b0cc5c1395b6978046feec45eb
Remus Selea <remus.selea@gmail.com>
2024-12-16 01:59:01 +0200

    Add bidirectional cursor infinite scroll example

--- examples/query/react/infinite-queries/src/App.tsx
+++ examples/query/react/infinite-queries/src/App.tsx
@@ -1,12 +1,14 @@
+import { BrowserRouter, Link, Route, Routes, useLocation } from "react-router"
 import "./App.css"
-import { BrowserRouter, Routes, Route, Link } from "react-router"
 
-import { PaginationExample } from "./features/pagination/PaginationExample"
+import { Outlet } from "react-router"
+import BidirectionalCursorInfScroll from "./features/bidirectional-cursor-infinite-scroll/BidirectionalCursorInfScroll"
 import {
-  InfiniteScrollExample,
   InfiniteScrollAbout,
+  InfiniteScrollExample,
 } from "./features/infinite-scroll/InfiniteScrollExample"
 import { InfiniteScrollMaxPagesExample } from "./features/max-pages/InfiniteScrollMaxExample"
+import { PaginationExample } from "./features/pagination/PaginationExample"
 
 const Menu = () => {
   return (
@@ -14,13 +16,20 @@ const Menu = () => {
       <h2>Examples</h2>
       <ul>
         <li>
-          <Link to="/pagination">Pagination</Link>
+          <Link to="/examples/pagination">Pagination</Link>
         </li>
         <li>
-          <Link to="/infinite-scroll">Infinite Scroll</Link>
+          <Link to="/examples/infinite-scroll">Infinite Scroll</Link>
         </li>
         <li>
-          <Link to="/infinite-scroll-max">Infinite Scroll + max pages</Link>
+          <Link to="/examples/infinite-scroll-max">
+            Infinite Scroll + max pages
+          </Link>
+        </li>
+        <li>
+          <Link to="/examples/bidirectional-cursor-infinte-scroll">
+            Bidirectional Cursor-Based Infinite Scroll
+          </Link>
         </li>
       </ul>
     </div>
@@ -32,18 +41,33 @@ const App = () => {
     <BrowserRouter>
       <div className="App">
         <h1>RTKQ Infinite Query Example Showcase</h1>
+
         <Routes>
           <Route path="/" element={<Menu />} />
-          <Route path="/pagination" element={<PaginationExample />} />
-          <Route path="/infinite-scroll" element={<InfiniteScrollExample />} />
           <Route
-            path="/infinite-scroll/about"
-            element={<InfiniteScrollAbout />}
-          />
-          <Route
-            path="/infinite-scroll-max"
-            element={<InfiniteScrollMaxPagesExample />}
-          />
+            path="/examples"
+            element={
+              <div>
+                <Link to="/">Back to Menu</Link>
+                <Outlet />
+              </div>
+            }
+          >
+            <Route path="pagination" element={<PaginationExample />} />
+            <Route path="infinite-scroll" element={<InfiniteScrollExample />} />
+            <Route
+              path="infinite-scroll/about"
+              element={<InfiniteScrollAbout />}
+            />
+            <Route
+              path="infinite-scroll-max"
+              element={<InfiniteScrollMaxPagesExample />}
+            />
+            <Route
+              path="bidirectional-cursor-infinte-scroll"
+              element={<BidirectionalCursorInfScroll />}
+            />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
