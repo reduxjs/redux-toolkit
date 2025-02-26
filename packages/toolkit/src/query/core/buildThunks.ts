bac32062f9ee49af36bd87094101f154d7a1d611
Ben Durrant <ben.durrant@marketdojo.com>
2025-02-26 14:53:27 +0000

    add result, error, and meta schemas

--- packages/toolkit/src/query/core/buildThunks.ts
+++ packages/toolkit/src/query/core/buildThunks.ts
@@ -503,10 +503,13 @@ export function buildThunks<
     },
   ) => {
     const endpointDefinition = endpointDefinitions[arg.endpointName]
+    const { metaSchema } = endpointDefinition
 
     try {
-      let transformResponse: TransformCallback =
-        getTransformCallbackForEndpoint(endpointDefinition, 'transformResponse')
+      let transformResponse = getTransformCallbackForEndpoint(
+        endpointDefinition,
+        'transformResponse',
+      )
 
       const baseQueryApi = {
         signal,
@@ -563,11 +566,10 @@ export function buildThunks<
         finalQueryArg: unknown,
       ): Promise<QueryReturnValue> {
         let result: QueryReturnValue
-        const { extraOptions, argSchema } = endpointDefinition
+        const { extraOptions, argSchema, rawResultSchema, resultSchema } =
+          endpointDefinition
 
-        if (argSchema) {
-          await parseWithSchema(argSchema, finalQueryArg)
-        }
+        if (argSchema) await parseWithSchema(argSchema, finalQueryArg)
 
         if (forceQueryFn) {
           // upsertQueryData relies on this to pass in the user-provided value
@@ -622,11 +624,24 @@ export function buildThunks<
 
         if (result.error) throw new HandledError(result.error, result.meta)
 
-        const transformedResponse = await transformResponse(
-          result.data,
+        let { data } = result
+
+        if (rawResultSchema) {
+          data = await parseWithSchema(rawResultSchema, result.data)
+        }
+
+        let transformedResponse = await transformResponse(
+          data,
           result.meta,
           finalQueryArg,
         )
+
+        if (resultSchema) {
+          transformedResponse = await parseWithSchema(
+            resultSchema,
+            transformedResponse,
+          )
+        }
 
         return {
           ...result,
@@ -717,6 +732,13 @@ export function buildThunks<
         finalQueryReturnValue = await executeRequest(arg.originalArgs)
       }
 
+      if (metaSchema && finalQueryReturnValue.meta) {
+        finalQueryReturnValue.meta = await parseWithSchema(
+          metaSchema,
+          finalQueryReturnValue.meta,
+        )
+      }
+
       // console.log('Final result: ', transformedData)
       return fulfillWithValue(
         finalQueryReturnValue.data,
@@ -726,25 +748,43 @@ export function buildThunks<
         }),
       )
     } catch (error) {
-      let catchedError = error
-      if (catchedError instanceof HandledError) {
-        let transformErrorResponse: TransformCallback =
-          getTransformCallbackForEndpoint(
-            endpointDefinition,
-            'transformErrorResponse',
-          )
+      let caughtError = error
+      if (caughtError instanceof HandledError) {
+        let transformErrorResponse = getTransformCallbackForEndpoint(
+          endpointDefinition,
+          'transformErrorResponse',
+        )
+        const { rawErrorSchema, errorSchema } = endpointDefinition
+
+        let { value, meta } = caughtError
+
+        if (rawErrorSchema) {
+          value = await parseWithSchema(rawErrorSchema, value)
+        }
+
+        if (metaSchema) {
+          meta = await parseWithSchema(metaSchema, meta)
+        }
 
         try {
+          let transformedErrorResponse = await transformErrorResponse(
+            value,
+            meta,
+            arg.originalArgs,
+          )
+          if (errorSchema) {
+            transformedErrorResponse = await parseWithSchema(
+              errorSchema,
+              transformedErrorResponse,
+            )
+          }
+
           return rejectWithValue(
-            await transformErrorResponse(
-              catchedError.value,
-              catchedError.meta,
-              arg.originalArgs,
-            ),
-            addShouldAutoBatch({ baseQueryMeta: catchedError.meta }),
+            transformedErrorResponse,
+            addShouldAutoBatch({ baseQueryMeta: meta }),
           )
         } catch (e) {
-          catchedError = e
+          caughtError = e
         }
       }
       if (
@@ -754,12 +794,12 @@ export function buildThunks<
         console.error(
           `An unhandled error occurred processing a request for the endpoint "${arg.endpointName}".
 In the case of an unhandled error, no tags will be "provided" or "invalidated".`,
-          catchedError,
+          caughtError,
         )
       } else {
-        console.error(catchedError)
+        console.error(caughtError)
       }
-      throw catchedError
+      throw caughtError
     }
   }
 
